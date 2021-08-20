f8d54d8f060df0961d5187e20cce7cd3efc6cd03
Jack Greenberg <j@jackgreenberg.co>
2021-08-19 20:35:30 -0400

    Fix formatting issues

--- examples/bazel_test/test_main.c
+++ examples/bazel_test/test_main.c
@@ -3,10 +3,10 @@
 #include <setjmp.h>
 #include <stdint.h>
 #include <cmocka.h>
-#include "library.h"
+#include "examples/bazel_test/library.h"
 
 static void test_sum(void** state) {
-    assert_int_equal(sum(1, 2), 3);    
+    assert_int_equal(sum(1, 2), 3);
 }
 
 int main(void) {
