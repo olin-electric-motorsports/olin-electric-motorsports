fafa89fa17e6520bf6943659fc4f0a1e8b3fc4c7
Jack Greenberg <j@jackgreenberg.co>
2021-08-23 19:44:57 -0400

    Fix clang format script

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
+# Ensure git-clang-format is installed
 if ! command -v git-clang-format &> /dev/null
 then
     echo "git-clang-format could not be found. Install with sudo apt install clang-format"
-    exit
+    exit 1
 fi
+
+exec 2>&1
 
 if [ "$1" = "run" ]
 then
-    out=$(git-clang-format -f --style file ./**/*)
-    if [ "$out" != "" ]
-    then
-        echo $out
-        exit 0
-    fi
+    git-clang-format -f --style=file --commit origin/main
 else
-    out=$(git-clang-format -f --diff --style file ./**/* -q)
+    out=$(git-clang-format -f --diff --commit origin/main --style=file -q)
     if [ "$out" != "no modified files to format" ]
     then
         echo "Formatting errors"
         exit 1
     fi
 fi
+
+exit 0
