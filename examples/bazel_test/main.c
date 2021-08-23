2834235d91d669e540e5ecb1af79e861d89ed2e0
Jack Greenberg <j@jackgreenberg.co>
2021-08-23 13:46:29 -0400

    Integrate initial CAN mock into example testing

--- examples/bazel_test/main.c
+++ examples/bazel_test/main.c
@@ -1,8 +1,9 @@
 #include "examples/bazel_test/library.h"
-#include <avr/io.h>
+#include "libs/can/api.h"
 
 int main(void) {
     // Setup
+    can_init(BAUD_500KBPS);
 
     while(1) {
         (void)sum(1, 2);
--- libs/can/mock/can.c
+++ libs/can/mock/can.c
@@ -13,16 +13,31 @@ void can_init(baud_rate_t baud) {
 }
 
 int can_send(can_frame_t* frame) {
-    check_expected(frame);
-    return 0;
+    check_expected(frame->id);
+    check_expected(frame->dlc);
+    check_expected(frame->data);
+    return mock();
 }
 
 int can_receive(can_frame_t* frame, can_filter_t filter) {
-    check_expected(frame);
-    return 0;
+    (void)frame;
+    check_expected(filter.id);
+    check_expected(filter.mask);
+    return mock();
 }
 
 int can_poll_complete(can_frame_t* frame) {
     check_expected(frame);
+
+    uint8_t id = mock_type(uint8_t);
+    frame->id = id;
+
+    uint8_t dlc = mock_type(uint8_t);
+    frame->dlc = dlc;
+
+    for (uint8_t i = 0; i < dlc; i++) {
+        frame->data[i] = mock_type(uint8_t);
+    }
+
     return mock();
 }
