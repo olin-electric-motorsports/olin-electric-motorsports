65f3307646340372f42db60e8713791e5ca5a565
Jack Greenberg <jgreenberg@olin.edu>
2021-08-24 08:23:20 -0700

    Fix clang format script (#15)
    There are two version of clang format: clang-format and git-clang-format. The former allows you to pass in files and it will just check the files. The latter, however, allows you to pass in a git commit SHA. It will then do a diff between the SHA you passed in and the HEAD (the most recent committed changes). It will then format those files, which means ultimately that it will only effect the parts of the files that were changed since the commit SHA.
    
    What this means for us is that we need to pass in the SHA of the place we are merging into, which we assume to be origin/main***. So now in CI we do a fetch of origin/main, and the script will run against the last SHA of origin/main.
    
    *** Note: This assumption might not always be valid, so ideally it would be best to check in a PR for the base of the PR (where we are merging back into) and get the SHA of it to pass in, but our solution works for now

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
+# Ensure git-clang-format is installed
 if ! command -v git-clang-format &> /dev/null
 then
     echo "git-clang-format could not be found. Install with sudo apt install clang-format"
-    exit
+    exit 1
 fi
 
-if [ "$1" = "run" ]
+if [ -z "$1" ]
 then
-    out=$(git-clang-format -f --style file ./**/*)
-    if [ "$out" != "" ]
+    echo "No command specified. Use either \"reformat\" or \"check\""
+    exit 1
+fi
+
+if [ "$1" = "check" ]
+then
+    commit_id=$(git show-ref ${GITHUB_BASE_REF:-origin/main} | awk '{print $1}')
+    out=$(git-clang-format -f --diff --commit $commit_id --style=file -q)
+
+    if [ "$out" != "no modified files to format" ] && [ "$out" != "" ]
     then
-        echo $out
-        exit 0
-    fi
-else
-    out=$(git-clang-format -f --diff --style file ./**/* -q)
-    if [ "$out" != "no modified files to format" ]
-    then
-        echo "Formatting errors"
+        echo "Formatting errors, run ./scripts/clang_format.sh reformat"
         exit 1
     fi
+
+    exit 0
+elif [ "$1" = "reformat" ]
+then
+    git-clang-format -f --style=file --commit origin/main
+else
+    echo "Command not recognized. Use either \"reformat\" or \"check\""
+    exit 1
 fi
