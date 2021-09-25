2a665a8de03742aece072aeaeaa1179e83aeaa3a
Jack Greenberg <j@jackgreenberg.co>
2021-09-25 12:07:20 -0400

    Update script to remove verbosity
    buildify

--- .github/git-bazel-kicad.sh
+++ .github/git-bazel-kicad.sh
@@ -7,7 +7,7 @@ if [[ ! -z $GITHUB_BASE_REF ]]; then
 fi
 
 files=()
-for file in $(git diff --name-only --diff-filter=ACMRT ${GITHUB_BASE_SHA:-"origin/main"} ${GITHUB_SHA:-$(git rev-parse HEAD)}); do
+for file in $(git diff --name-only --diff-filter=ACMRT ${GITHUB_BASE_SHA:-"origin/main"} ${GITHUB_SHA:-$(git rev-parse HEAD)} | grep "kicad_pcb$\|sch$"); do
     files+=($(bazelisk query --keep_going --noshow_progress $file))
 done
 
@@ -17,48 +17,57 @@ buildables=$(bazelisk query \
     "kind(kibot, rdeps(//..., set(${files[*]})))" 2>/dev/null)
 
 if [[ ! -z $buildables ]]; then
-    bazelisk build --config=docker-kicad --build_tag_filters=kicad $buildables 2>/dev/null
+    echo "Building:"
+    echo "${buildables}"
+    bazelisk build \
+        --config=docker-kicad \
+        --noshow_progress \
+        --build_tag_filters=kicad \
+        $buildables 2>/dev/null
+
+    # Doesn't include tar file
+    basenames=()
+    rm -rf build
+    mkdir -p build
+
+    for file in $buildables; do
+        file="${file//://}"
+        cp $(bazelisk info bazel-genfiles)/${file:2} build/
+
+        basenames+=($(basename $file))
+    done
+
+    rm -rf build/comment.md
+
+    echo "Creating GH comment"
+    echo "# KiCad Artifacts" >> build/comment.md
+
+    echo "<details><summary>File links</summary><ul>" >> build/comment.md
+    for file in build/*; do
+        if [[ ! $file == "build/comment.md" ]]; then
+            url="https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/$(basename $file)"
+            echo "<li><a href=\"$url\">$(basename $file)</a></li>" >> build/comment.md
+        fi
+    done
+
+    echo "</ul></details>" >> build/comment.md
+
+    echo "Converting SVGs to use white backgrounds"
+    for file in build/*.svg; do
+        mv ${file} ${file}_old
+        rsvg-convert -b white -f svg ${file}_old > ${file}
+        rm -rf ${file}_old
+    done
+
+    for file in build/*_sch.svg; do
+        echo "<p align=\"center\"><img src=\"https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/$(basename $file)\" width=\"100%\"/></p>" >> build/comment.md
+    done
+
+    for file in build/*_pcb.svg; do
+        echo "<p align=\"center\">" >> build/comment.md
+        echo "<img src=\"https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/$(basename $file)\" width=\"60%\"/>" >> build/comment.md
+        echo "</p>" >> build/comment.md
+    done
+else
+    echo "Nothing to build"
 fi
-
-# Doesn't include tar file
-basenames=()
-rm -rf build
-mkdir -p build
-for file in $buildables; do
-    file="${file//://}"
-    cp $(bazelisk info bazel-genfiles)/${file:2} build/
-
-    basenames+=($(basename $file))
-done
-
-rm -rf build/comment.md
-
-echo "# KiCad Artifacts" >> build/comment.md
-
-echo "<details><summary>File links</summary><ul>" >> build/comment.md
-for file in build/*; do
-    if [[ ! $file == "build/comment.md" ]]; then
-        url="https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/$(basename $file)"
-        echo "<li><a href=\"$url\">$(basename $file)</a></li>" >> build/comment.md
-    fi
-done
-
-echo "</ul></details>" >> build/comment.md
-
-for file in build/*_sch.svg; do
-    mv ${file} ${file}_old
-    rsvg-convert -b white -f svg ${file}_old > ${file}
-    rm -rf ${file}_old
-
-    echo "<p align=\"center\"><img src=\"https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/$(basename $file)\" width=\"100%\"/></p>" >> build/comment.md
-done
-
-for file in build/*_pcb.svg; do
-    mv ${file} ${file}_old
-    rsvg-convert -b white -f svg ${file}_old > ${file}
-    rm -rf ${file}_old
-
-    echo "<p align=\"center\">" >> build/comment.md
-    echo "<img src=\"https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/$(basename $file)\" width=\"60%\"/>" >> build/comment.md
-    echo "</p>" >> build/comment.md
-done
