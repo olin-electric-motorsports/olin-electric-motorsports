2718d2c7645ec9e575bf4a230ab557fe0b5daa22
Jack Greenberg <j@jackgreenberg.co>
2021-09-27 20:33:32 -0400

    clang-format

--- projects/btldr/libs/image/api.h
+++ projects/btldr/libs/image/api.h
@@ -3,21 +3,21 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define IMAGE_MAGIC (0x4F454D21)  // "OEM!"
+#define IMAGE_MAGIC (0x4F454D21) // "OEM!"
 
-#define IMAGE_VALID (0x0)
-#define IMAGE_INVALID_CRC (0x1)
+#define IMAGE_VALID         (0x0)
+#define IMAGE_INVALID_CRC   (0x1)
 #define IMAGE_INVALID_MAGIC (0x2)
 
-#define CHIP_NONE (0x000U)
-#define CHIP_AVR_ATMEGA16M1 (0x001U)
-#define CHIP_AVR_ATMEGA328P (0x002U)
+#define CHIP_NONE              (0x000U)
+#define CHIP_AVR_ATMEGA16M1    (0x001U)
+#define CHIP_AVR_ATMEGA328P    (0x002U)
 #define CHIP_ARM_STM32F103C8T6 (0x003U)
 
 typedef struct __attribute__((packed)) {
     uint32_t image_magic;
     uint32_t crc;
-    uint16_t image_size;  // size without header
+    uint16_t image_size; // size without header
     uint64_t flash_timestamp;
     uint32_t reserved;
     char git_sha[8];
