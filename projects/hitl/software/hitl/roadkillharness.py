9b2f25b31883a508e7c66830f1f971f77ee3c7cb
Alex Wenstrup <awenstrup@olin.edu>
2021-09-30 22:01:29 -0400

    First pass at CANController refactor

--- projects/hitl/software/hitl/roadkillharness.py
+++ projects/hitl/software/hitl/roadkillharness.py
@@ -5,7 +5,6 @@ import configparser
 from typing import Optional
 
 # Project imports
-from hitl.ecu import ECU
 from hitl.utils import get_logging_config, root_path, artifacts_path
 from hitl.iocontroller import IOController
 from hitl.cancontroller import CANController
@@ -43,35 +42,9 @@ class RoadkillHarness:
             )
         )
 
-        # Create all ECUs
-        ecus = {}
-
-        self.log.info("Creating throttle ecu...")
-        self.throttle = ECU(name="throttle", io=self.io)
-        ecus["throttle"] = self.throttle
-
-        self.log.info("Creating dashboard ecu...")
-        self.dashboard = ECU(name="dashboard", io=self.io)
-        ecus["dashboard"] = self.dashboard
-
-        self.log.info("Creating air_ctrl ecu...")
-        self.air_ctrl = ECU(name="air_ctrl", io=self.io)
-        ecus["air_ctrl"] = self.air_ctrl
-
-        self.log.info("Creating bms_core ecu...")
-        self.bms_core = ECU(name="bms_core", io=self.io)
-        ecus["bms_core"] = self.bms_core
-
-        self.log.info("Creating brakelight_bspd ecu...")
-        self.brakelight_bspd = ECU(name="brakelight_bspd", io=self.io)
-        ecus["brakelight_bspd"] = self.brakelight_bspd
-
-        # Add more ECUs here
-
         # Create CANController
         self.log.info("Creating CANController...")
         self.can = CANController(
-            ecus=ecus,
             can_spec_path=os.path.join(artifacts_path, config.get("PATHS", "dbc_path")),
             channel=config.get("HARDWARE", "can_channel", fallback="vcan0"),
             bitrate=config.get("HARDWARE", "can_bitrate", fallback="500000"),
