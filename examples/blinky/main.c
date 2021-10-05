0b1bf526120d0f1a5ff7fd4482b97d95789a8e38
Jack Greenberg <j@jackgreenberg.co>
2021-10-05 13:03:53 -0400

    Refactor GPIO library for different use

--- examples/blinky/main.c
+++ examples/blinky/main.c
@@ -2,14 +2,10 @@
 #include <avr/io.h>
 #include "libs/gpio/api.h"
 #include "libs/uart/api.h"
+#include "libs/gpio/pin_defs.h"
 #include "config.h"
 
-gpio_t LED0 = {
-    .num = PB0,
-    .ddr = DDRB,
-    .port = PORTB,
-    .pin = PINB,
-};
+gpio_t LED0 = PB2;
 
 int main(void) {
     gpio_set_mode(LED0, OUTPUT);
