9f6c305a2ed2d63022dfc1cc0ef868d5fc7efb54
Jack Greenberg <j@jackgreenberg.co>
2021-10-20 11:17:51 -0400

    Add unfinished state diagram and some code

---
+++ vehicle/mkv/software/air_control/config.h
@@ -0,0 +1,10 @@
+#include "libs/gpio/api.h"
+
+/*
+ * Pin definitions
+ */
+gpio_t SS_PRECHARGECTL = PB2;
+gpio_t SS_TSMS = PB3;
+gpio_t BMS_SENSE = PC0;
+gpio_t AIR_P_WELD_DETECT = PC4;
+gpio_t AIR_N_WELD_DETECT = PC5;
