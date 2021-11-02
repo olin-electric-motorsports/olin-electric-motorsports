93dcabfe42423881df7cb04efa8b397231b0d1a4
Jack Greenberg <j@jackgreenberg.co>
2021-11-02 15:08:05 -0400

    Add comments

--- libs/gpio/api.h
+++ libs/gpio/api.h
@@ -10,6 +10,9 @@ typedef struct {
     uint8_t pin;
 } gpio_t;
 
+/*
+ * Set a GPIO pin to be an input or an output
+ */
 #define gpio_set_mode(gpio_pin, mode)                       \
     do {                                                    \
         if (mode == OUTPUT) {                               \
@@ -19,11 +22,35 @@ typedef struct {
         }                                                   \
     } while (0)
 
+/*
+ * Set a gpio pin high
+ */
 #define gpio_set_pin(gpio_pin) (_SFR_IO8(gpio_pin.port) |= (1 << gpio_pin.num))
+
+/*
+ * Set a gpio pin low
+ */
 #define gpio_clear_pin(gpio_pin) \
     (_SFR_IO8(gpio_pin.port) &= ~(1 << gpio_pin.num))
+
+/*
+ * Toggle a gpio pin (high becomes low, low becomes high)
+ */
 #define gpio_toggle_pin(gpio_pin) \
     (_SFR_IO8(gpio_pin.port) ^= (1 << gpio_pin.num))
+
+/*
+ * Get the value of a GPIO pin
+ */
 #define gpio_get_pin(gpio_pin) (_SFR_IO8(gpio_pin.pin) & (1 << gpio_pin.num))
 
+/*
+ * Enable interrupts for the given pin. The interrupt function can then be
+ * defined like so:
+ *
+ * ```
+ * void pcint0_callback(void) {
+ *     // Do interrupt stuff
+ * }
+ */
 void gpio_enable_interrupt(gpio_t pin);
--- libs/gpio/gpio.c
+++ libs/gpio/gpio.c
@@ -4,13 +4,26 @@
 
 #include "libs/gpio/api.h"
 
-#define NUM_PIN_INTERRUPTS (26)
-
 /*
- * List of function pointers to store the callbacks
+ * Placeholder functions for interrupt callbacks for the pin change interrupt
+ * vectors.
+ *
+ * The string `__attribute__((weak))` tells the compiler that there are default
+ * functions that are defined here, but if someone implements them elsewhere,
+ * use the other ones.
+ *
+ * So we have these defined as weak so that the code will always compile, and
+ * then in your code file (like `air_control.c`), you can define a function with
+ * the same name:
+ *
+ * ```
+ * void pcint0_callback(void) {
+ *     // Decide which pin changed, and do something
+ * }
+ * ```
+ *
+ * This is how interrupts work in this library.
  */
-// void (*callbacks[NUM_PIN_INTERRUPTS])(void);
-
 __attribute__((weak)) void pcint0_callback(void) {};
 __attribute__((weak)) void pcint1_callback(void) {};
 __attribute__((weak)) void pcint2_callback(void) {};
@@ -38,25 +51,18 @@ void gpio_enable_interrupt(gpio_t pin) {
         case 0x05: {
             PCICR |= (1 << PCIE0);
             PCMSK0 |= (1 << pin.num);
-            // callbacks[pin.num] = callback;
         } break;
         case 0x08: {
             PCICR |= (1 << PCIE1);
             PCMSK1 |= (1 << pin.num);
-            // callbacks[pin.num + 8] = callback;
         } break;
         case 0x0B: {
             PCICR |= (1 << PCIE2);
             PCMSK2 |= (1 << pin.num);
-            // callbacks[pin.num + 16] = callback;
         } break;
         case 0x0E: {
             PCICR |= (1 << PCIE3);
             PCMSK3 |= (1 << pin.num);
-            // callbacks[pin.num + 24] = callback;
-        } break;
-        default: {
-            // Nothing
         } break;
     }
 }
