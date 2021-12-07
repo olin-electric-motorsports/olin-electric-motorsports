f6863bdfa3fc6c87f33338ae918dc34408269ef1
Krishna Suresh <krishna4132@gmail.com>
2021-12-06 20:42:10 -0500

    fixed script for action runner

---
+++ projects/hitl/software/scripts/hitl_bashrc_warning.py
@@ -0,0 +1,6 @@
+import os
+import warnings
+warnings.warn("The Action Runner status is below, before running any test scripts make sure the runner is inactive by running sudo ./svc.sh stop")
+sudo_password = 'formula'
+command = "./svc.sh status"
+os.system('echo %s|sudo -S %s' % (sudo_password, command))
--- projects/hitl/software/scripts/hitl_control.py
+++ projects/hitl/software/scripts/hitl_control.py
@@ -1,12 +1,11 @@
 import os
 import RPi.GPIO as GPIO
-import asyncio
 from enum import Enum
 
-sudo_password = 'formula' # not a good way of doing
+sudo_password = 'formula' 
 runner_on = False # default to false since action runner is off when script is ran for the first time
-TOGGLE_PIN = 1
-LED_PIN = 2
+TOGGLE_PIN = 1 # GPIO pin on the raspberry pi for the toggle button
+LED_PIN = 2 # GPIO pin on the raspberry pi for the indicator LED
 def set_runner(new_state):
     '''Starts or stops the action runner based on the value of runner_on'''
     global runner_on
@@ -22,33 +21,12 @@ def action_runner_button_callback():
     Toggles the action runner on and off
     '''
     global runner_on
-    set_runner(not runner_on)
+    set_runner(not runner_on) # Switches the state of the action runner
 
 def set_indicator_LED(LED_on):
     '''Sets the LED to the pattern corresponding to the action runner state'''
     GPIO.output(LED_PIN, 1 if LED_on else 0)
 
-# def is_manual_active():
-#     return False
 
-# def is_ssh_active():
-#     return os.system('ss | grep ssh')
-
-# async def manual_connection_checker():
-#     '''Checks if a direct or ssh connection is found'''
-#     await asyncio.sleep(1000)
-#     if os.system('w'):
-#         os.environ["MANUAL"] = True
-#         set_runner(False)
-#     else:
-#         del os.environ["MANUAL"]
-#         if is_ssh_active():
-#             set_runner(False)
-#         else:
-#             set_runner(True)
-
-
-GPIO.setup(LED_PIN, GPIO.OUT)
-GPIO.add_interrupt_callback(TOGGLE_PIN, action_runner_button_callback, edge='rising', debounce_timeout_ms=100)
-# loop = asyncio.get_event_loop()
-# loop.run_forever(manual_connection_checker)
+GPIO.setup(LED_PIN, GPIO.OUT) # Enable the LED pin
+GPIO.add_interrupt_callback(TOGGLE_PIN, action_runner_button_callback, edge='rising', debounce_timeout_ms=100) # Sets the toggle button to call action_runner_button_callback() on press
