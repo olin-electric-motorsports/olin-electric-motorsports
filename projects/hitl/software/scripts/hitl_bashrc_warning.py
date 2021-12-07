73a7d29d172c95de85a64e72dd58d083b0cf2432
Krishna Suresh <krishna4132@gmail.com>
2021-12-06 20:49:48 -0500

    added script description

--- projects/hitl/software/scripts/hitl_bashrc_warning.py
+++ projects/hitl/software/scripts/hitl_bashrc_warning.py
@@ -1,5 +1,6 @@
 import os
 import warnings
+# Script used to warn users of the status of the action runner
 warnings.warn("The Action Runner status is below, before running any test scripts make sure the runner is inactive by running sudo ./svc.sh stop")
 sudo_password = 'formula'
 command = "./svc.sh status"
