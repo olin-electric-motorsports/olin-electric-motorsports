039cfff350fd60fc5f0ad89f79b916bf334ab453
Krishna Suresh <krishna4132@gmail.com>
2021-12-09 20:32:42 -0500

    action runner button works

--- projects/hitl/software/scripts/hitl_bashrc_warning.py
+++ projects/hitl/software/scripts/hitl_bashrc_warning.py
@@ -1,7 +1,5 @@
-import os
-import warnings
 # Script used to warn users of the status of the action runner
-warnings.warn("The Action Runner status is below, before running any test scripts make sure the runner is inactive by running sudo ./svc.sh stop")
-sudo_password = 'formula'
-command = "./svc.sh status"
-os.system('echo %s|sudo -S %s' % (sudo_password, command))
+import os
+print("Please check the status of the Action Runner by running cd /home/pi/actions-runner/; sudo ./svc.sh status. Before running any test scripts make sure the runner is inactive by running sudo ./svc.sh stop")
+command = f'sudo ./svc.sh status'
+os.system('cd /home/pi/actions-runner; echo %s|sudo -S %s' % ('formula', command))
--- projects/hitl/software/scripts/hitl_control.py
+++ projects/hitl/software/scripts/hitl_control.py
@@ -1,24 +1,26 @@
+#!/usr/bin/env python
 import os
 import RPi.GPIO as GPIO
 from enum import Enum
+import time
+import signal
+import sys
 
-sudo_password = 'formula' 
-runner_on = False # default to false since action runner is off when script is ran for the first time
-TOGGLE_PIN = 1 # GPIO pin on the raspberry pi for the toggle button
-LED_PIN = 2 # GPIO pin on the raspberry pi for the indicator LED
 def set_runner(new_state):
     '''Starts or stops the action runner based on the value of runner_on'''
+    global runner_on
     if runner_on is not new_state:
         runner_on = new_state
         action = 'start' if new_state else 'stop'
         command = f'sudo ./svc.sh {action}'
-        os.system('echo %s|sudo -S %s' % (sudo_password, command))
+        os.system('cd /home/pi/actions-runner; echo %s|sudo -S %s' % (sudo_password, command))
         set_indicator_LED(new_state)
 
-def action_runner_button_callback():
+def action_runner_button_callback(channel):
     '''
     Toggles the action runner on and off
     '''
+    global runner_on
     set_runner(not runner_on) # Switches the state of the action runner
 
 def set_indicator_LED(LED_on):
@@ -26,5 +28,18 @@ def set_indicator_LED(LED_on):
     GPIO.output(LED_PIN, 1 if LED_on else 0)
 
 
-GPIO.setup(LED_PIN, GPIO.OUT) # Enable the LED pin
-GPIO.add_interrupt_callback(TOGGLE_PIN, action_runner_button_callback, edge='rising', debounce_timeout_ms=100) # Sets the toggle button to call action_runner_button_callback() on press
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    sys.exit(0)
+if __name__ == '__main__':
+    sudo_password = 'formula' 
+    runner_on = False # default to false since action runner is off when script is ran for the first time
+    TOGGLE_PIN = 16 # GPIO pin on the raspberry pi for the toggle button
+    LED_PIN = 12 # GPIO pin on the raspberry pi for the indicator LED
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(TOGGLE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(LED_PIN, GPIO.OUT) # Enable the LED pin
+    GPIO.add_event_detect(TOGGLE_PIN, GPIO.FALLING, callback=action_runner_button_callback, bouncetime=200)
+    
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()
