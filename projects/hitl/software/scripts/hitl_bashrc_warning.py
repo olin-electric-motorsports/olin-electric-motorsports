3256cc4222a08acb09a6b556508e4ccf723c0bd0
Krishna Suresh <krishna4132@gmail.com>
2021-12-09 21:16:47 -0500

    changed warning msg

--- projects/hitl/software/scripts/hitl_bashrc_warning.py
+++ projects/hitl/software/scripts/hitl_bashrc_warning.py
@@ -1,5 +1,5 @@
 # Script used to warn users of the status of the action runner
 import os
-print("Please check the status of the Action Runner by running cd /home/pi/actions-runner/; sudo ./svc.sh status. Before running any test scripts make sure the runner is inactive by running sudo ./svc.sh stop")
 command = f'sudo ./svc.sh status'
 os.system('cd /home/pi/actions-runner; echo %s|sudo -S %s' % ('formula', command))
+print("Above is the status of the action runner. Before running any test scripts make sure the runner is inactive by running sudo ./svc.sh stop")
