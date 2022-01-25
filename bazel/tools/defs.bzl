6269ec4b112f999aab8ae7381a98876d9e91b3a2
Jack Greenberg <jgreenberg@olin.edu>
2022-01-24 18:55:58 -0800

    CAN API and DBC generator (#86)

---
+++ bazel/tools/defs.bzl
@@ -0,0 +1,65 @@
+load("@rules_cc//cc:defs.bzl", "cc_library")
+
+def dbc_gen(name, srcs):
+    native.genrule(
+        name = name,
+        srcs = srcs,
+        outs = [
+            name,
+        ],
+        tools = [
+            "//projects/can_api:dbc_generator",
+        ],
+        cmd = "$(location //projects/can_api:dbc_generator) -o $(OUTS) $(SRCS)",
+        visibility = ["//visibility:public"],
+    )
+
+def can_api_files(name, yaml, dbc):
+    native.genrule(
+        name = name + "_tools",
+        srcs = [
+            dbc,
+        ],
+        outs = [
+            "can_tools.c",
+            "can_tools.h",
+        ],
+        tools = [
+            "@cantools_//:tools",
+        ],
+        cmd = "./$(location @cantools_//:tools) generate_c_source --database-name can_tools --output-directory $(RULEDIR) $(location " + dbc + ")",
+    )
+
+    native.genrule(
+        name = name + "_gen",
+        srcs = [
+            dbc,
+            yaml,
+            "//projects/can_api/files/c_templates:c_file.j2",
+            "//projects/can_api/files/c_templates:h_file.j2",
+        ],
+        outs = [
+            "can_api.c",
+            "can_api.h",
+        ],
+        tools = [
+            "//projects/can_api:c_generator",
+        ],
+        cmd = "$(location //projects/can_api:c_generator) -o $(RULEDIR) -y $(location " + yaml + ") -d $(location " + dbc + ") -c $(location //projects/can_api/files/c_templates:c_file.j2) -H $(location //projects/can_api/files/c_templates:h_file.j2)",
+    )
+
+    cc_library(
+        name = name,
+        srcs = [
+            ":can_tools.c",
+            ":can_api.c",
+        ],
+        hdrs = [
+            ":can_tools.h",
+            ":can_api.h",
+        ],
+        deps = [
+            "//libs/can",
+        ],
+        visibility = ["//visibility:public"],
+    )
---
+++ projects/can_api/yaml_handler.py
@@ -0,0 +1,113 @@
+"""
+Author: Hwei-Shin Harriman, Jack Greenberg
+Olin Electric Motorsports
+
+Functionality to process mini-YAML files, autogenerate mini-YAMLS with ID
+assignments and RX messages definitions, and create DBCs
+"""
+import yaml
+import math
+from cantools.database.can import Message as CANMessage, Signal as MessageSignal, Node
+
+#----------- DEFAULTS and CONSTANTS -----------
+VALID_PRIORITIES = ["LO", "MED", "HI"]
+
+class YamlParser:
+    """
+    Handles parsing of YAML file
+
+    Usage:
+
+        parser = YamlHandler("path/to/yaml_file.yml")
+    """
+    def __init__(self, yml):
+        with open(yml, 'r') as f:
+            self.data = yaml.load(f, Loader=yaml.FullLoader)
+
+        self.name = self.data["name"]
+        self.messages = self._parse_messages()
+
+    """
+    Parses messages from the YAML file. Internally it will pull the frequency
+    and message IDs, and then call _parse_signals to get the list of signals
+    """
+    def _parse_messages(self):
+        messages = []
+        if "publish" not in self.data.keys():
+            return []
+
+        for raw in self.data["publish"]:
+
+            id = raw["id"]
+            signals, length = self._parse_signals(raw)
+            cycle_time = (1 / raw["freq_hz"]) * 1000 # Milliseconds
+
+            m = CANMessage(
+                id,
+                raw["name"],
+                length,
+                signals,
+                senders = [self.name],
+                cycle_time = cycle_time,
+            )
+
+            messages.append(m)
+
+        return messages
+
+    """
+    Parses signals from the YAML file. The possible types are currently: enum,
+    int8_t, int16_t, uint8_t, uint16_t, and bool.
+    """
+    def _parse_signals(self, msg):
+        signals = []
+        message_length = 0
+
+        if "signals" not in msg.keys():
+            return [], 0
+
+        for sig in msg["signals"]:
+            name = sig["name"]
+            start, length = sig["slice"].split(" + ")
+
+            scale = 1
+            offset = 0
+
+            sig_type = sig["unit"]["type"]
+
+            unit = None
+            is_signed = False
+            choices = None
+
+            if sig_type == "enum":
+                choices = dict(enumerate(sig["unit"]["values"]))
+            elif sig_type == "uint8_t" or sig_type == "uint16_t":
+                unit = sig["unit"]["name"]
+                offset = eval(str(sig["unit"]["offset"]))
+                scale = eval(str(sig["unit"]["scale"]))
+            elif sig_type == "int8_t" or sig_type == "int16_t":
+                is_signed = True
+                unit = sig["unit"]["name"]
+                offset = eval(str(sig["unit"]["offset"]))
+                scale = eval(str(sig["unit"]["scale"]))
+            elif sig_type == "bool":
+                unit = "bool"
+                choices = {0: sig["unit"]["values"]["f"], 1: sig["unit"]["values"]["t"]}
+            else:
+                raise Exception("Unknown type: {}".format(sig_type))
+
+            s = MessageSignal(
+                name,
+                int(start),
+                int(length),
+                is_signed = is_signed,
+                scale = scale,
+                offset = offset,
+                unit = unit,
+                choices = choices,
+            )
+
+            signals.append(s)
+            message_length += int(length) / 8
+
+        return signals, math.ceil(message_length)
