e335ce0df04008869baa437cb84b22a86dcb8cc5
Krishna Suresh <krishna4132@gmail.com>
2022-01-25 11:31:54 -0500

    cleaned up code and restructured

---
+++ projects/microhitl/ShellIOControl/ShellIOControl.ino
@@ -0,0 +1,88 @@
+#include <Shell.h>
+
+void setup()
+{
+  Serial.begin(9600);
+  shell_init(shell_reader, shell_writer, 0);
+  shell_register(gpio_mode, PSTR("gpio_mode"));
+  shell_register(gpio_read, PSTR("gpio_read"));
+  shell_register(gpio_write, PSTR("gpio_write"));
+}
+
+void loop()
+{
+  shell_task();
+}
+
+int gpio_mode(int argc, char** argv)
+{
+  if (argc != 3) {
+    shell_print_error(E_SHELL_ERR_ARGCOUNT,0);
+    shell_println("");
+    return SHELL_RET_FAILURE;
+  }
+  int pin =strtol(argv[1],0,0);
+  if(!strcmp(argv[2], (const char *) "INPUT"))
+    pinMode(pin, INPUT);
+  else if(!strcmp(argv[2], (const char *) "OUTPUT")) 
+    pinMode(pin, OUTPUT);
+    
+  return SHELL_RET_SUCCESS;
+}
+
+int gpio_read(int argc, char** argv)
+{
+  if (argc != 2) {
+    shell_print_error(E_SHELL_ERR_ARGCOUNT,0);
+    shell_println("");
+    return SHELL_RET_FAILURE;
+  }
+  int pin =strtol(argv[1],0,0);
+  if(digitalRead(pin) == HIGH)
+    shell_println("read:HIGH");
+  else
+    shell_println("read:LOW");
+  return SHELL_RET_SUCCESS;
+}
+
+int gpio_write(int argc, char** argv)
+{
+  if (argc != 3) {
+    shell_print_error(E_SHELL_ERR_ARGCOUNT,0);
+    shell_println("");
+    return SHELL_RET_FAILURE;
+  }
+  int pin =strtol(argv[1],0,0);
+  if(!strcmp(argv[2], (const char *) "HIGH"))
+    digitalWrite(pin,HIGH);
+  else if(!strcmp(argv[2], (const char *) "LOW")) 
+    digitalWrite(pin,LOW);
+  return SHELL_RET_SUCCESS;
+}
+
+
+/**
+ * Function to read data from serial port
+ * Functions to read from physical media should use this prototype:
+ * int my_reader_function(char * data)
+ */
+int shell_reader(char * data)
+{
+  // Wrapper for Serial.read() method
+  if (Serial.available()) {
+    *data = Serial.read();
+    return 1;
+  }
+  return 0;
+}
+
+/**
+ * Function to write data to serial port
+ * Functions to write to physical media should use this prototype
+ * void my_writer_function(char data)
+ */
+void shell_writer(char data)
+{
+  // Wrapper for Serial.write() method
+  Serial.write(data);
+}
--- projects/microhitl/microhitl.py
+++ projects/microhitl/microhitl.py
@@ -1,4 +1,3 @@
-from traceback import print_tb
 import serial
 from enum import Enum
 import time
@@ -14,7 +13,7 @@ class Values(Enum):
     LOW = "LOW"
 
 
-class Arduino():
+class MicroHitl():
     def __init__(self, port='/dev/ttyACM0', baudrate=9600, timeout=.1):
         self.arduino = serial.Serial(
             port=port, baudrate=baudrate, timeout=timeout)
@@ -22,21 +21,16 @@ class Arduino():
     def write_pin(self, pin: int, output_value: Values):
         '''Writes to arduino pin with Value of HIGH or LOW'''
         self.arduino.write(
-            f"ioctrl -p {pin} -w {output_value.value}\r".encode('utf-8'))
+            f"gpio_write {pin} {output_value.value}\r".encode('utf-8'))
 
     def read_pin(self, pin: int):
         '''Reads from arduino pin and return true if output contains HIGH'''
-        self.arduino.write(f"ioctrl -p {pin} -r \r".encode('utf-8'))
-        self.arduino.readline()
-        output = self.arduino.readline()
+        self.arduino.write(f"gpio_read {pin}\r".encode('utf-8'))
+        output = self.arduino.read(50)
         print(output)
         return 'read:HIGH' in output.decode("utf-8")
 
     def configure_pin(self, pin: int, pin_type: PinTypes):
         '''Configures arduino pins as INPUT or OUTPUT'''
         self.arduino.write(
-            f"ioctrl -p {pin} -m {pin_type.value}\r".encode('utf-8'))
-        if (pin_type is PinTypes.INPUT):
-            time.sleep(0.5)
-            for i in range(10):
-                self.read_pin(pin)
+            f"gpio_mode {pin} {pin_type.value}\r".encode('utf-8'))
