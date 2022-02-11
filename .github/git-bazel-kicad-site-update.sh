44f59d4fd47d661c4c7606f984c023183f2a2afb
achakraborty <achakraborty@olin.edu>
2022-02-10 22:06:31 -0500

    debug .sh

--- .github/git-bazel-kicad-site-update.sh
+++ .github/git-bazel-kicad-site-update.sh
@@ -2,6 +2,8 @@ if [[ ! -z $GITHUB_BASE_REF ]]; then
     git fetch origin $GITHUB_BASE_REF
 fi
 
+echo "Yo I am here"
+
 GITHUB_SHA=${GITHUB_SHA:=$(git rev-parse HEAD)}
 
 files=()
@@ -9,9 +11,17 @@ for file in $(git diff --name-only --diff-filter=ACMRT ${GITHUB_BASE_SHA:-"origi
     files+=($(bazel query --keep_going --noshow_progress $file))
 done
 
+echo "Check Here"
+
 buildables=$(bazel query --keep going --noshow_progress "kind(kibot, rdeps(//..., set(${files[*]})))" 2>/dev/null)
+
+echo "Before Buildables"
+echo "${buildables}"
+echo "After Buildables"
 
 if [[ ! -z $buildables ]]; then 
     echo "Building:"
     echo "${buildables}"
 fi
+
+echo "End of File"
