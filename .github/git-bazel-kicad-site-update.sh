458b9df8f438d6aad8e04608fb7685a47f327329
achakraborty <achakraborty@olin.edu>
2022-02-11 03:06:45 -0500

    testing

--- .github/git-bazel-kicad-site-update.sh
+++ .github/git-bazel-kicad-site-update.sh
@@ -15,13 +15,13 @@ echo "${GITHUB_SHA:-$(git rev-parse HEAD)}"
 
 files=()
 for file in $(git diff --name-only --diff-filter=ACMRT ${GITHUB_BASE_SHA:-"origin/main"} ${GITHUB_SHA:-$(git rev-parse HEAD)} | grep "kicad_pcb$\|sch$"); do
-    files+=($(bazel query --keep_going --noshow_progress $file))
+    files+=($(bazelisk query --keep_going --noshow_progress $file))
 done
 
 echo "${Files}"
 echo "Check Here"
 
-buildables=$(bazel query --keep going --noshow_progress "kind(kibot, rdeps(//..., set(${files[*]})))" 2>/dev/null)
+buildables=$(bazelisk query --keep going --noshow_progress "kind(kibot, rdeps(//..., set(${files[*]})))" 2>/dev/null)
 
 echo "Before Buildables"
 echo "${buildables}"
