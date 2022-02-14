315dc832c8babd1ffc0b8781821eaa0738b5392b
achakraborty <achakraborty@olin.edu>
2022-02-13 21:09:14 -0500

    removed comments

--- .github/git-bazel-kicad-site-update.sh
+++ .github/git-bazel-kicad-site-update.sh
@@ -1,35 +1,23 @@
-#!/bin/bash
-
-# Script to build all KiCad targets that have been changed in the PR
-
-# Used in CI to fetch origin/main so that there is something to compare changed
-# files to
 if [[ ! -z $GITHUB_BASE_REF ]]; then
     git fetch origin $GITHUB_BASE_REF
 fi
 echo "Beginning of File"
-# Set the HEAD Git commit (the current commit)
+
 GITHUB_SHA=${GITHUB_SHA:=$(git rev-parse HEAD)}
 
-# Creates a list of .kicad_pcb and .sch files that have changed since the
-# GITHUB_BASE_REF
 files=()
 for file in $(git diff --name-only --diff-filter=ACMRT ${GITHUB_BASE_SHA:-"origin/main"} ${GITHUB_SHA:-$(git rev-parse HEAD)} | grep "kicad_pcb$\|sch$"); do
     files+=($(bazelisk query --keep_going --noshow_progress $file))
 done
 
-# Gets a list of Bazel targets that include the files from above
 buildables=$(bazelisk query \
     --keep_going \
     --noshow_progress \
     "kind(kibot, rdeps(//..., set(${files[*]})))" 2>/dev/null)
 
-echo "${Buildables}"
-
-# Only build if there are things that have changed
 if [[ ! -z $buildables ]]; then
-    echo "Building:"
+    echo "Files to update in site:"
     echo "${buildables}"
 else
-    echo "Nothing to build"
+    echo "Nothing to update"
 fi
