5d36bbce4da6bc444f0a265ecded54b528af9ed0
Amit Kumar <51718056+amit-kumarh@users.noreply.github.com>
2022-02-25 10:51:31 -0500

    Update dashboard firmware to use new libraries (#89)
    * initial commit
    
    * first pass
    
    * adding semicolons
    
    * Make dashboard code compile
    
    * Added RTD Buzzer
    
    * Added RTD Buzzer
    
    * second pass
    
    * cleaning up and removing old code
    
    * clean up
    
    * Fix pcint, rename header
    
    * reformatted code with script
    
    * reformat BUILD file
    
    * remove old dashboard.h
    
    * dashboard yaml
    
    * git build issue
    
    * Fix dashboard YML file and buildify
    
    * first pass at new can lib
    
    * small fixes
    
    * fix formatting
    
    * upload dummy files to make code compile
    
    * fix dummy files and reformat
    
    * first round of testing, can_poll_receive refactor
    
    * merge changes from main
    
    * fixed CAN lib build file
    
    * Fix rebase issues
    
    * Fix rebase conflicts part 2
    
    Co-authored-by: Alex Wenstrup <awenstrup@olin.edu>
    Co-authored-by: Jack Greenberg <j@jackgreenberg.co>

---
+++ vehicle/mkv/software/dashboard/dashboard_config.h
@@ -0,0 +1,37 @@
+#include "libs/adc/api.h"
+#include "libs/can/api.h"
+#include "libs/gpio/api.h"
+#include "libs/gpio/pin_defs.h"
+#include "libs/timer/api.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+#define RTD_BUZZ_TIME 400 // hundredths of a second
+
+// Pin definitions
+gpio_t IMD_LED = PB3;
+gpio_t START_LED = PB4;
+gpio_t BMS_LED = PD0;
+gpio_t START_BTN = PC6;
+gpio_t HV_LED = PD7;
+gpio_t LV_LED = PD6;
+gpio_t BRAKE_LED = PB2;
+gpio_t RTD_BUZZER_LSD = PC5;
+
+// ADC PIN
+adc_pin_e STEERING_POS = ADC2;
+
+// Timer setup
+void timer0_callback(void);
+
+timer_cfg_s timer0_cfg = {
+    .timer = TIMER0,
+    .timer0_mode = TIMER0_MODE_CTC,
+    .prescalar = CLKIO_DIV_1024,
+    .channel_a = {
+        .output_compare_match = 0x27,
+        .pin_behavior = DISCONNECTED,
+        .interrupt_enable = true,
+        .interrupt_callback = timer0_callback,
+    },
+};
