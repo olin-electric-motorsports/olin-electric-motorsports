221321a11cfc068238b519b5e6bf9a91384712ec
Jack Greenberg <j@jackgreenberg.co>
2022-03-27 15:55:07 -0400

    Add comments for rules

--- vehicle/mkv/software/throttle/throttle.c
+++ vehicle/mkv/software/throttle/throttle.c
@@ -12,19 +12,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define IMPLAUSIBILITY_TIMEOUT (100) // 10 * 10ms
-#define MOTOR_ANTICLOCKWISE    (1)
+#define IMPLAUSIBILITY_TIMEOUT_MS (100)
+#define MOTOR_ANTICLOCKWISE       (1)
 
 // Set this to lower than 10 to limit torque request (i.e. 5 limits to half of
 // max torque)
 #define TORQUE_REQUEST_SCALE (10)
 
-// TODO: comment and add reference to rules (25% of (256 - 1))
+// Represents value for 25% pedal travel to check for brake implausibility
 #define APPS_BRAKE_IMPLAUSIBILITY_THRESHOLD (63)
 
-// floor(5% of (256 - 1))
+// Represents value for 5% pedal travel to check for brake implausibility
 #define APPS_BRAKE_IMPLAUSIBILITY_THRESHOLD_LOW (12)
 
+// Represents value for 10% deviation between throttle sensors
 #define APPS_IMPLAUSIBILITY_DEVIATION_THRESHOLD (25)
 
 /*
@@ -81,19 +82,25 @@ static int16_t get_throttle_travel(const throttle_potentiometer_s* throttle) {
     // Pedal position percentage (between 0 and 1)
     int16_t position_pct = (throttle_raw - throttle->throttle_min) / range;
 
-    // NOTE: If we decide to do a non-linear map, that would go here
     return (position_pct * 255);
 }
 
+/*
+ * Checks whether either throttle sensor reads an out-of-range value.
+ *
+ * See 2022 Rules T.4.2.10 and T.4.2.9.
+ */
 static bool check_out_of_range(int16_t* pos_r, int16_t* pos_l) {
     // Check range of positions
     if (*pos_r > 255) {
         // Out of range upper
         *pos_r = 255;
+        gpio_set_pin(OUT_OF_RANGE_IMPLAUSIBILITY_LED);
         return true;
     } else if (*pos_r < 0) {
         // Out of range lower
         *pos_r = 0;
+        gpio_set_pin(OUT_OF_RANGE_IMPLAUSIBILITY_LED);
         return true;
     }
 
@@ -104,25 +111,42 @@ static bool check_out_of_range(int16_t* pos_r, int16_t* pos_l) {
     if (*pos_l > 255) {
         // Out of range upper
         *pos_l = 255;
+        gpio_set_pin(OUT_OF_RANGE_IMPLAUSIBILITY_LED);
         return true;
     } else if (*pos_l < 0) {
         // Out of range lower
         *pos_l = 0;
+        gpio_set_pin(OUT_OF_RANGE_IMPLAUSIBILITY_LED);
         return true;
     }
 
     return false;
 }
 
+/*
+ * Checks for deviation between the two positions reported by the throttle
+ * sensors. Deviation greater than 10% is defined as an implausibility.
+ *
+ * See 2022 Rules T.4.2.4 and T.4.2.5.
+ */
 static bool check_deviation(int16_t pos_max, int16_t pos_min) {
     // Check implausibility between pedal values (T.4.2.4/5)
     if (pos_max - pos_min > APPS_IMPLAUSIBILITY_DEVIATION_THRESHOLD) {
+        gpio_set_pin(DEVIATION_IMPLAUSIBILITY_LED);
         return true;
     } else {
         return false;
     }
 }
 
+/*
+ * Checks for plausibility with brake being pressed while throttle is also
+ * pressed.
+ *
+ * See 2022 Rules EV.5.7. When the brake is pressed and the throttle pedal is
+ * pressed beyond 25% pedal travel, we immediately set the torque request to
+ * zero and maintain that until throttle pedal is pressed less than 5%.
+ */
 static bool check_brake(int16_t pos_min) {
     // Check brake plausibility (EV.5.7)
     static bool brake_implausibility_occured = false;
@@ -130,6 +154,7 @@ static bool check_brake(int16_t pos_min) {
     if (throttle_state.brake_pressed) {
         if (pos_min >= APPS_BRAKE_IMPLAUSIBILITY_THRESHOLD) {
             brake_implausibility_occured = true;
+            gpio_set_pin(BRAKE_IMPLAUSIBILTIY_LED);
             return true;
         }
 
@@ -149,11 +174,11 @@ int main(void) {
     adc_init();
     timer_init(&timer0_cfg);
 
-    gpio_set_mode(DEBUG_LED1, OUTPUT);
-    gpio_set_mode(DEBUG_LED2, OUTPUT);
-    gpio_set_mode(DEBUG_LED3, OUTPUT);
-    gpio_set_mode(LED1, OUTPUT);
-    gpio_set_mode(LED2, OUTPUT);
+    gpio_set_mode(BRAKE_IMPLAUSIBILTIY_LED, OUTPUT);
+    gpio_set_mode(DEVIATION_IMPLAUSIBILITY_LED, OUTPUT);
+    gpio_set_mode(OUT_OF_RANGE_IMPLAUSIBILITY_LED, OUTPUT);
+    gpio_set_mode(RJ45_LED1, OUTPUT);
+    gpio_set_mode(RJ45_LED2, OUTPUT);
 
     gpio_set_mode(SS_ESTOP, INPUT);
     gpio_set_mode(SS_IS, INPUT);
@@ -205,19 +230,24 @@ int main(void) {
 
             int16_t pos_r = get_throttle_travel(&throttle_r);
             int16_t pos_l = get_throttle_travel(&throttle_l);
+
             int16_t pos_min = MIN(pos_r, pos_l);
             int16_t pos_max = MAX(pos_r, pos_l);
+
+            if (check_brake(pos_min)) {
+                SET_TORQUE_REQUEST(0);
+                continue;
+            }
 
             bool implausibility = false;
             implausibility |= check_out_of_range(&pos_r, &pos_l);
             implausibility |= check_deviation(pos_max, pos_min);
-            implausibility |= check_brake(pos_min);
 
             if (implausibility) {
                 throttle_state.implausibility_fault_counter++;
 
                 if (throttle_state.implausibility_fault_counter
-                    >= IMPLAUSIBILITY_TIMEOUT) {
+                    >= IMPLAUSIBILITY_TIMEOUT_MS) {
                     SET_TORQUE_REQUEST(0);
                     throttle_state.implausibility_fault_counter = 0;
                 } // else maintain previous throttle request
@@ -227,6 +257,7 @@ int main(void) {
                 // With no implausibility...
                 throttle_state.implausibility_fault_counter = 0;
 
+                // NOTE: If we decide to do a non-linear map, that would go here
                 int16_t torque_request = pos_min * TORQUE_REQUEST_SCALE;
 
                 SET_TORQUE_REQUEST(torque_request);
