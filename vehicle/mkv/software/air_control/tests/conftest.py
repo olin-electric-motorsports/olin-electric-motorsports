d190d35c0280bbb88ba9b2c50feec81bda4c2781
Jack Greenberg <j@jackgreenberg.co>
2022-04-05 09:07:39 -0700

    Add AIR Control code (#91)
    * Initial state machine
    
    * Initial new AIR code
    
    * Add unfinished state diagram and some code
    
    * Update state machine with enums
    
    * Create new AIR control code files
    
    * WIP
    
    * Remove old state machine library
    
    * Refactor state machine into 1ms task
    
    * Add code for new CAN address space
    
    This won't be merged in, I just wanted to try writing some code and want
    to commit it so that it exists in my repository.
    
    * Update fault list
    
    * Set state variable to match CAN message
    
    * Update AIR control code with new CAN API
    
    * Finish updating state machine to use new CAN API
    
    * Small updates and tweaks
    
    * Add INIT state
    
    * Add pseudocode tests for air control
    
    * Move can_send to main loop
    
    * Implement SS_CLOSED state and add comments
    
    * Small fixes
    
    * Remove unused variable
    
    * Update CAN library to test CAN API
    
    * Update AIR Control to fix HITL tests
    
    * All AIR Control HitL tests pass
    
    * Format
    
    * Update discharge to check for MC timeout
    
    * fmt
    
    * Remove outdated diagrams
    
    * Update with PR feedback
    
    * Fix shared timer variable name
    
    * Rename bsp to utils
    
    * Add AIR_ prefix to faults
    
    * Fix duplicate variable
    
    * Add clarifying comments
    
    * Rename variables and add comments
    
    * fmt

---
+++ vehicle/mkv/software/air_control/tests/conftest.py
@@ -0,0 +1,62 @@
+import os
+import pytest
+import time
+from formula.projects.hitl.software.hitl import CANController
+from formula.projects.microhitl import MicroHitl, Values as PinValue, PinTypes
+
+# Allow passing in arguments
+def pytest_addoption(parser):
+    parser.addoption("--canbus", action="store", default="can0")
+    parser.addoption("--hitl-port", action="store", default="/dev/ttyACM0")
+
+
+@pytest.fixture
+def candevice(request):
+    return request.config.getoption("--canbus")
+
+
+@pytest.fixture
+def hitl_port(request):
+    return request.config.getoption("--hitl-port")
+
+
+@pytest.fixture
+def pins():
+    return {
+        "PRECHARGE_CTL": [3, PinTypes.INPUT],  # PB2
+        "AIR_N_LSD": [5, PinTypes.INPUT],  # PC6
+        "FAULT_LED": [10, PinTypes.OUTPUT],  # PB7
+        "SS_TSMS": [6, PinTypes.OUTPUT],  # PB3
+        "SS_IMD_LATCH": [7, PinTypes.OUTPUT],  # PB4
+        "SS_MPC": [12, PinTypes.OUTPUT],  # PB5
+        "SS_HVD_CONN": [11, PinTypes.OUTPUT],  # PB6
+        "RESET": [2, PinTypes.OUTPUT],
+        "BMS_SENSE": [8, PinTypes.OUTPUT],  # PC0
+        "AIR_P_WELD_DETECT": [4, PinTypes.OUTPUT],  # PC4
+        "AIR_N_WELD_DETECT": [13, PinTypes.OUTPUT],  # PC7
+        "IMD_SENSE": [9, PinTypes.OUTPUT],  # PD0
+    }
+
+
+@pytest.fixture
+def iocontroller(hitl_port, pins):
+    ioctrl = MicroHitl(hitl_port)
+    time.sleep(2)
+
+    for idx, (name, info) in enumerate(pins.items()):
+        ioctrl.configure_pin(info[0], info[1])
+
+    ioctrl.write_pin(pins["RESET"][0], PinValue.HIGH)
+
+    return ioctrl
+
+
+@pytest.fixture
+def canbus(candevice):
+    busname = candevice or "can0"
+
+    can = CANController("vehicle/mkv/mkv.dbc", busname, 500000)
+
+    yield can
+
+    can.stop_periodic()
