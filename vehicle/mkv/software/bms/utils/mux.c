2f1356d334c870280df5708a43a041f914506c5f
Jack Greenberg <j@jackgreenberg.co>
2022-04-06 00:17:26 -0400

    Refactor mux library for memory usage

--- vehicle/mkv/software/bms/utils/mux.c
+++ vehicle/mkv/software/bms/utils/mux.c
@@ -2,9 +2,10 @@
 
 #include <stdbool.h>
 
+#include "vehicle/mkv/software/bms/bms_config.h"
 #include "vehicle/mkv/software/bms/ltc6811/ltc6811.h"
 
-#define MUX_DATALENGTH (3)
+#define MUX_DATALENGTH (3 * 3) // 3 bytes of data for 3 muxes each
 
 // ICOMn bits
 #define START       (0b0110 << 4)
@@ -17,31 +18,39 @@
 #define NACK      (0b1000)
 #define NACK_STOP (0b1001)
 
+#define CMD_LEN (6)
+
 void enable_mux(uint8_t num_ics, cell_asic ics[], uint8_t address, bool enable,
                 uint8_t channel) {
-    uint8_t cmd = enable ? 0x8 : 0x00;
-    cmd |= channel & 0x7;
+    uint8_t tx_data[CMD_LEN] = { 0 };
+
+    /*
+     * Third byte is command for mux: xxxxECCC
+     *   x: don't care
+     *   E: enable
+     *   C: channel
+     */
+    uint8_t mux_cmd = enable ? 0x8 : 0x00;
+    mux_cmd |= channel & 0x7;
 
     for (uint8_t ic = 0; ic < num_ics; ic++) {
         // Do nothing for the first byte transmitted
-        ics[ic].com.tx_data[0] = START;
-        ics[ic].com.tx_data[1] = NACK_STOP;
+        tx_data[0] = START;
+        tx_data[1] = NACK_STOP;
 
         // Second byte is address of mux
-        ics[ic].com.tx_data[2] = START | (address >> 4);
-        ics[ic].com.tx_data[3] = (address << 4) | NACK;
+        tx_data[2] = START | (address >> 4);
+        tx_data[3] = (address << 4) | NACK;
 
-        // Third byte is command for mux: xxxxECCC
-        //   x: don't care
-        //   E: enable
-        //   C: channel
-        ics[ic].com.tx_data[4] = START;
-        ics[ic].com.tx_data[5] = (cmd << 4) | NACK_STOP;
+        tx_data[4] = START;
+        tx_data[5] = (mux_cmd << 4) | NACK_STOP;
     }
 
     wakeup_sleep(num_ics);
-    LTC6811_wrcomm(num_ics, ics);
+
+    uint8_t wrcomm_cmd[2] = { 0x07, 0x21 };
+    write_68(NUM_ICS, wrcomm_cmd, tx_data);
 
     wakeup_idle(num_ics);
-    LTC6811_stcomm(MUX_DATALENGTH); // TODO: what len do we do?
+    LTC6811_stcomm(MUX_DATALENGTH);
 }
