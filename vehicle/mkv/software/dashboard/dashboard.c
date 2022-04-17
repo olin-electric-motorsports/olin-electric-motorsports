1885b83fb219be6e9f4872e979df4b1eeff7d7c3
Jack Greenberg <j@jackgreenberg.co>
2022-04-16 21:31:48 -0400

    Fixes from integration testing

--- vehicle/mkv/software/dashboard/dashboard.c
+++ vehicle/mkv/software/dashboard/dashboard.c
@@ -20,18 +20,18 @@
 #include "vehicle/mkv/software/dashboard/can_api.h"
 
 #define BMS_FAULT_NONE (0)
+#define AIR_STATE_TS_ACTIVE (4)
 
 volatile bool START_BUTTON_STATE;
 volatile bool HV_STATE;
 volatile bool BRAKE_PRESSED;
-volatile bool READYTODRIVE;
 
 volatile bool send_can;
 volatile int buzzer_counter = 0;
 
 // Start Button interrupt & final ReadyToDrive check
 void pcint1_callback(void) {
-    START_BUTTON_STATE = !!gpio_get_pin(START_BTN);
+    START_BUTTON_STATE = !gpio_get_pin(START_BTN);
     dashboard.start_button_state = START_BUTTON_STATE;
 }
 
@@ -43,7 +43,6 @@ int main(void) {
     // Initialization
     can_init(BAUD_500KBPS);
     timer_init(&timer0_cfg);
-    adc_init();
 
     // Set pin modes
     gpio_set_mode(IMD_LED, OUTPUT);
@@ -55,6 +54,9 @@ int main(void) {
     gpio_set_mode(RTD_BUZZER_LSD, OUTPUT);
 
     gpio_set_mode(START_BTN, INPUT);
+    gpio_set_pin(START_BTN); // Enable internal pull-up resistor
+
+    pcint1_callback(); // Set initial condition of hardware
 
     // Enable interrupts
     sei();
@@ -63,18 +65,12 @@ int main(void) {
     // Turn on LV LED
     gpio_set_pin(LV_LED);
 
-    // Set Error Code
-    dashboard.fault_code = 0;
-
     // Receive CAN Messages
     can_receive_brakelight();
     can_receive_bms_core();
     can_receive_air_control_critical();
-    for (;;) {
-        dashboard.steering_position = adc_read(STEERING_POS);
 
-        // Brakelight message for Brakelight LED and
-        // check
+    for (;;) {
         if (can_poll_receive_brakelight() == 0) {
             if (brakelight.brake_gate) {
                 BRAKE_PRESSED = true;
@@ -83,6 +79,7 @@ int main(void) {
                 BRAKE_PRESSED = false;
                 gpio_clear_pin(BRAKE_LED);
             }
+
             can_receive_brakelight();
         }
 
@@ -92,22 +89,20 @@ int main(void) {
                 == BMS_FAULT_NONE) { // check BMS status
                 gpio_set_pin(BMS_LED); // set BMS light high
             } else {
-                gpio_clear_pin(BMS_LED); // clear BMS light
+                gpio_clear_pin(BMS_LED); // BMS OFF means NOT OK
             }
+
+            // Receive again
             can_receive_bms_core();
         }
 
-        // AIR Control Critical message for HV LED and disabling ReadyToDrive if
-        // HV goes down and IMD Status LED
         if (can_poll_receive_air_control_critical() == 0) {
-            if (!air_control_critical.air_p_status
-                && !air_control_critical.air_n_status) {
+            if (air_control_critical.state == AIR_STATE_TS_ACTIVE) {
                 HV_STATE = true;
                 gpio_set_pin(HV_LED); // set HV LED
             } else {
                 HV_STATE = false;
-                READYTODRIVE = false; // Disable RTD
-                dashboard.ready_to_drive = false;
+                dashboard.ready_to_drive = false; // Disable RTD
                 gpio_clear_pin(HV_LED); // clear HV LED
                 gpio_clear_pin(START_LED); // clear Start LED
                 buzzer_counter = 0; // reset counter for next RTD cycle
@@ -121,15 +116,14 @@ int main(void) {
             can_receive_air_control_critical();
         }
 
-        // Throttle message for interfacing with LED Bars Board - TODO (SPI
-        // library)
-        // if (can_receive_throttle() == 0) {
-        // do some stuff here
-        // }
+        if (BRAKE_PRESSED && HV_STATE && !dashboard.ready_to_drive) {
+            gpio_set_pin(START_LED);
+        } else {
+            gpio_clear_pin(START_LED);
+        }
 
         if (START_BUTTON_STATE && HV_STATE && BRAKE_PRESSED) {
-            gpio_set_pin(START_LED);
-            READYTODRIVE = true;
+            gpio_clear_pin(START_LED);
             dashboard.ready_to_drive = true;
             gpio_set_pin(RTD_BUZZER_LSD); // turn on RTD Buzzer
         }
@@ -139,7 +133,7 @@ int main(void) {
             send_can = false;
 
             // Uses timer to measure the 4 seconds to activate the RTD buzzer
-            if (READYTODRIVE && (buzzer_counter < RTD_BUZZ_TIME)) {
+            if (dashboard.ready_to_drive && (buzzer_counter < RTD_BUZZ_TIME)) {
                 buzzer_counter++;
             }
             if (buzzer_counter >= RTD_BUZZ_TIME) {
