8434406964dcfe91b71796ffc904ec0f6517940e
Jack Greenberg <j@jackgreenberg.co>
2022-05-04 20:45:53 -0400

    Add SeeedStudio CAN bridge

---
+++ tools/can/bridge.py
@@ -0,0 +1,32 @@
+import can
+import threading
+import time
+
+seeedbus = can.interface.Bus(bustype='seeedstudio', channel='/dev/ttyUSB0', bitrate=500000)
+virt = can.interface.Bus('vcan0', bustype='virtual', interface="socketcan", preserve_timestamps=True)
+
+def seeed_to_virt():
+    for message in seeedbus:
+        virt.send(message)
+        time.sleep(0.001)
+
+    while True:
+        sleep(1)
+
+def virt_to_seeed():
+    for message in virt:
+        seeedbus.send(message)
+        time.sleep(0.001)
+
+    while True:
+        sleep(1)
+
+if __name__ == "__main__":
+    sv = threading.Thread(target=seeed_to_virt, daemon=True)
+    vs = threading.Thread(target=virt_to_seeed, daemon=True)
+
+    sv.start()
+    vs.start()
+
+    while True:
+        time.sleep(1)
