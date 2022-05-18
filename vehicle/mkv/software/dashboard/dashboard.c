417a386b65dbde24ac5e8023606e4f4be5436776
Jack Greenberg <j@jackgreenberg.co>
2022-05-18 14:04:18 -0700

    Add btldr to dashboard

--- vehicle/mkv/software/dashboard/dashboard.c
+++ vehicle/mkv/software/dashboard/dashboard.c
@@ -19,6 +19,18 @@
 #include "libs/timer/api.h"
 #include "vehicle/mkv/software/dashboard/can_api.h"
 
+#include "projects/btldr/btldr_lib.h"
+#include "projects/btldr/git_sha.h"
+#include "projects/btldr/libs/image/api.h"
+
+/*
+ * Required for btldr
+ */
+image_hdr_t image_hdr __attribute__((section(".image_hdr"))) = {
+    .image_magic = IMAGE_MAGIC,
+    .git_sha = STABLE_GIT_COMMIT,
+};
+
 #define BMS_FAULT_NONE (0)
 #define AIR_STATE_TS_ACTIVE (4)
 
