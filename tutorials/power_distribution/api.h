725d98eebeeeb3f1dbac1d1471ff1cd4214df316
Melissa Kazazic <mkazazic01@gmail.com>
2022-07-17 23:16:22 -0400

    Complete power distribution library

---
+++ tutorials/power_distribution/api.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "libs/gpio/api.h"
+#include "libs/gpio/pin_defs.h"
+
+// Pins
+
+gpio_t DASHBOARD_POWER =        PB0;
+gpio_t SERVICE_SECTION_POWER =  PB1;
+gpio_t THROTTLE_POWER =         PB2;
+
+// Macros
+
+/*
+Set power GPIO pin high.
+*/
+void power_device(gpio_t gpio_pin);
+
+/*
+Set dashboard power, service section power, and 
+throttle power GPIO pins high.
+*/
+void power_all();
+
+/*
+Set power GPIO pin low.
+*/
+void power_off_device(gpio_t gpio_pin);
+
+/*
+Set dashboard power, service section power, and 
+throttle power GPIO pins low.
+*/
+void power_off_all();
---
+++ tutorials/power_distribution/power_distribution.c
@@ -0,0 +1,37 @@
+#include "api.h"
+
+// Functions
+
+/*
+Set power GPIO pin high.
+*/
+void power_device(gpio_t gpio_pin) {
+    gpio_set_pin(gpio_pin);
+}
+
+/*
+Set dashboard power, service section power, and 
+throttle power GPIO pins high.
+*/
+void power_all(){
+    gpio_set_pin(DASHBOARD_POWER);
+    gpio_set_pin(SERVICE_SECTION_POWER);
+    gpio_set_pin(THROTTLE_POWER); 
+}
+
+/*
+Set power GPIO pin low.
+*/
+void power_off_device(gpio_t gpio_pin){
+    gpio_clear_pin(gpio_pin);
+}
+
+/*
+Set dashboard power, service section power, and 
+throttle power GPIO pins low.
+*/
+void power_off_all(){
+    gpio_clear_pin(DASHBOARD_POWER);
+    gpio_clear_pin(SERVICE_SECTION_POWER);
+    gpio_clear_pin(THROTTLE_POWER);
+}
