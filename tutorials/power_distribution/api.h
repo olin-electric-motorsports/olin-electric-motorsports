6ab1a727007f8ad0f09244dd3af58e259abf7aa1
Elvis Wolcott <ewolcott@olin.edu>
2022-07-17 21:22:16 -0400

    complete tutorial 1

---
+++ tutorials/power_distribution/api.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "libs/gpio/api.h"
+#include "libs/gpio/pin_defs.h"
+
+// pin defs
+gpio_t DASHBOARD_POWER = PB0;
+gpio_t SERVICE_SECTION_POWER = PB1;
+gpio_t THROTTLE_POWER = PB2;
+
+/* Power ON a device
+ *
+ * warning: board may exhibit unexpected behavior if DEVICE is not DASHBOARD_POWER, SERVICE_SECTION_POWER, or THROTTLE_POWER
+ */
+void power_device(gpio_t device);
+
+/* Power ON all devices
+ */
+void power_all();
+
+/* Power OFF a device
+ *
+ * warning: board may exhibit unexpected behavior if DEVICE is not DASHBOARD_POWER, SERVICE_SECTION_POWER, or THROTTLE_POWER
+ */
+void power_off_device(gpio_t device);
+
+/* Power OFF all devices
+ */
+void power_off_all();
---
+++ tutorials/power_distribution/power_distribution.c
@@ -0,0 +1,22 @@
+#include "tutorials/power_distribution/api.h"
+
+
+void power_device(gpio_t device) {
+    gpio_set_pin(device);
+}
+
+void power_all() {
+    power_device(DASHBOARD_POWER);
+    power_device(SERVICE_SECTION_POWER);
+    power_device(THROTTLE_POWER);
+}
+
+void power_off_device(gpio_t device) {
+    gpio_clear_pin(device);
+}
+
+void power_off_all() {
+    power_off_device(DASHBOARD_POWER);
+    power_off_device(SERVICE_SECTION_POWER);
+    power_off_device(THROTTLE_POWER);
+}
