8a9a064b5d579af7966f183f49ebdc0447bb0745
Melissa Kazazic <mkazazic01@gmail.com>
2022-07-17 23:34:35 -0400

    Reformat files w clang

--- tutorials/power_distribution/api.h
+++ tutorials/power_distribution/api.h
@@ -4,9 +4,9 @@
 
 // Pins
 
-gpio_t DASHBOARD_POWER =        PB0;
-gpio_t SERVICE_SECTION_POWER =  PB1;
-gpio_t THROTTLE_POWER =         PB2;
+gpio_t DASHBOARD_POWER = PB0;
+gpio_t SERVICE_SECTION_POWER = PB1;
+gpio_t THROTTLE_POWER = PB2;
 
 // Macros
 
@@ -16,7 +16,7 @@ Set power GPIO pin high.
 void power_device(gpio_t gpio_pin);
 
 /*
-Set dashboard power, service section power, and 
+Set dashboard power, service section power, and
 throttle power GPIO pins high.
 */
 void power_all();
@@ -27,7 +27,7 @@ Set power GPIO pin low.
 void power_off_device(gpio_t gpio_pin);
 
 /*
-Set dashboard power, service section power, and 
+Set dashboard power, service section power, and
 throttle power GPIO pins low.
 */
 void power_off_all();
--- tutorials/power_distribution/power_distribution.c
+++ tutorials/power_distribution/power_distribution.c
@@ -10,27 +10,27 @@ void power_device(gpio_t gpio_pin) {
 }
 
 /*
-Set dashboard power, service section power, and 
+Set dashboard power, service section power, and
 throttle power GPIO pins high.
 */
-void power_all(){
+void power_all() {
     gpio_set_pin(DASHBOARD_POWER);
     gpio_set_pin(SERVICE_SECTION_POWER);
-    gpio_set_pin(THROTTLE_POWER); 
+    gpio_set_pin(THROTTLE_POWER);
 }
 
 /*
 Set power GPIO pin low.
 */
-void power_off_device(gpio_t gpio_pin){
+void power_off_device(gpio_t gpio_pin) {
     gpio_clear_pin(gpio_pin);
 }
 
 /*
-Set dashboard power, service section power, and 
+Set dashboard power, service section power, and
 throttle power GPIO pins low.
 */
-void power_off_all(){
+void power_off_all() {
     gpio_clear_pin(DASHBOARD_POWER);
     gpio_clear_pin(SERVICE_SECTION_POWER);
     gpio_clear_pin(THROTTLE_POWER);
