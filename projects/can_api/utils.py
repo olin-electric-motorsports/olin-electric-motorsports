a8b2a22086e9fafb7997f6dbd0ff576e14b67046
ayushchakra <achakraborty@olin.edu>
2022-07-29 22:23:13 -0400

    updated c file generation with configurable mask

--- projects/can_api/utils.py
+++ projects/can_api/utils.py
@@ -5,10 +5,12 @@ def get_rx_messages(subs, messages):
     """
     rx_message_names = [d["name"] for d in subs]
     rx_messages = list(filter(lambda m: m.name in rx_message_names, messages))
+    masks = {subscriber['name']: subscriber['mask'] if 'mask' in subscriber.keys() else 0x7ff for \
+        subscriber in subs}
 
     mobs = {}
 
     for m in subs:
         mobs[m["name"]] = m["mob"]
 
-    return rx_messages, mobs
+    return rx_messages, mobs, masks
