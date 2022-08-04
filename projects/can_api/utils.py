f002f0fc7f4ce55ec7f740b2cd00eaf3170be697
ayushchakra <achakraborty@olin.edu>
2022-08-04 14:43:36 -0400

    updated error catching for unknown subsriptions

--- projects/can_api/utils.py
+++ projects/can_api/utils.py
@@ -1,21 +1,25 @@
+import pdb
+
+
 def get_rx_messages(subs, messages):
     """
     Gets the received Messages from the list of messages, based on the subs
     dictionary.
     """
-    rx_message_names = [d["name"] for d in subs]
+    # Check to see if subs has a message not specified in the DBC
+    dbc_message_names = [dbc_message.name for dbc_message in messages]
+    for message in subs:
+        if message["name"] not in dbc_message_names:
+            raise ValueError(
+                "Found subscription message that is not in database messages."
+            )
+
+    rx_message_names = [d['name'] for d in subs]
     rx_messages = list(filter(lambda m: m.name in rx_message_names, messages))
     masks = {message["name"]: message.get("mask", 0x7FF) for message in subs}
 
     mobs = {}
-
     for m in subs:
         mobs[m["name"]] = m["mob"]
-
-    for mask_subscription_name in masks.keys():
-        if mask_subscription_name not in rx_message_names:
-            raise ValueError(
-                "Found subscription message that is not in database messages."
-            )
 
     return rx_messages, mobs, masks
