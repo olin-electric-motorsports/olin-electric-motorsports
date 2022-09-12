40f39c60563d3fcfb80399b7615a893f3a8fc6aa
Jack Greenberg <j@jackgreenberg.co>
2022-09-12 00:26:58 -0400

    fmt

--- vehicle/mkv/software/bms/tools/charger_can_bridge.py
+++ vehicle/mkv/software/bms/tools/charger_can_bridge.py
@@ -3,11 +3,13 @@ import threading
 import time
 
 charger_fbk_filter = [
-    {"can_id": 0x18ff50e5, "can_mask": 0x7FFFFFFF, "extended": True},
+    {"can_id": 0x18FF50E5, "can_mask": 0x7FFFFFFF, "extended": True},
 ]
 
 vehicle_bus = can.interface.Bus(
-    bustype="socketcan", channel="can0", bitrate=500000
+    bustype="socketcan",
+    channel="can0",
+    bitrate=500000
     # can_filters = charger_fbk_filter
 )
 
@@ -20,9 +22,12 @@ virt = can.interface.Bus(
 # ]
 
 charging_bus = can.interface.Bus(
-    bustype="seeedstudio", channel="/dev/ttyUSB0", bitrate=250000
+    bustype="seeedstudio",
+    channel="/dev/ttyUSB0",
+    bitrate=250000
     # can_filters = vehicle_charging_cmd_filter
 )
+
 
 def charging_to_vehicle():
     try:
@@ -36,6 +41,7 @@ def charging_to_vehicle():
         print("PANIC")
         os._exit(0)
 
+
 def vehicle_to_charging():
     try:
         for message in vehicle_bus:
@@ -45,11 +51,12 @@ def vehicle_to_charging():
             if message.arbitration_id == 0x80:
 
                 # Repackage (change id and extended bool)
-                message.arbitration_id = 0x1806e5f4
+                message.arbitration_id = 0x1806E5F4
                 message.is_extended_id = True
                 charging_bus.send(message)
     except:
         os._exit(0)
+
 
 if __name__ == "__main__":
     cv = threading.Thread(target=charging_to_vehicle, daemon=True)
