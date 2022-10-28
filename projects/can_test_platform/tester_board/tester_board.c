4dece15691e63aabc0dd1aecfc489bf7e70dcd2f
ayushchakra <achakraborty@olin.edu>
2022-10-28 02:33:15 -0400

    --wip-- [skip ci]

--- projects/can_test_platform/tester_board/tester_board.c
+++ projects/can_test_platform/tester_board/tester_board.c
@@ -1,5 +1,6 @@
 #include "libs/timer/api.h"
 #include "tester_board_config.h"
+#include "projects/can_test_platform/tester_board/can_api.h"
 
 volatile bool send_can;
 
@@ -9,4 +10,16 @@ void timer0_isr(void) {
 
 int main(void) {
     sei();
+    can_init_can_test_platform();
+    timer_init(&timer0_cfg);
+    can_receive_throttle();
+
+    if (can_poll_receive_throttle() == 0) {
+        can_receive_throttle();
+    }
+    
+    if (send_can) {
+        send_can = false;
+        can_send_can_test_platform();
+    }
 }
