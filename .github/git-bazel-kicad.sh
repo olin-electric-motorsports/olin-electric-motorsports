27e9622ea11c417aad1a5e58b4771ab811990ef8
ayushchakra <achakraborty@olin.edu>
2022-11-23 22:07:41 -0500

    testing python script in github actions

--- .github/git-bazel-kicad.sh
+++ .github/git-bazel-kicad.sh
@@ -1,104 +1,106 @@
 #!/bin/bash
 
-# Script to build all KiCad targets that have been changed in the PR
+python3 test.py
 
-# Used in CI to fetch origin/main so that there is something to compare changed
-# files to
-if [[ ! -z $GITHUB_BASE_REF ]]; then
-    git fetch origin $GITHUB_BASE_REF
-fi
+# # Script to build all KiCad targets that have been changed in the PR
 
-# Set the HEAD Git commit (the current commit)
-GITHUB_SHA=${GITHUB_SHA:=$(git rev-parse HEAD)}
+# # Used in CI to fetch origin/main so that there is something to compare changed
+# # files to
+# if [[ ! -z $GITHUB_BASE_REF ]]; then
+#     git fetch origin $GITHUB_BASE_REF
+# fi
 
-# Creates a list of .kicad_pcb and .kicad_sch files that have changed since the
-# GITHUB_BASE_REF
-files=()
-for file in $(find vehicle/mkv -type f | grep "kicad_pcb$\|kicad_sch$"); do
-    files+=($(bazel query --keep_going --noshow_progress $file))
-done
+# # Set the HEAD Git commit (the current commit)
+# GITHUB_SHA=${GITHUB_SHA:=$(git rev-parse HEAD)}
 
-# Gets a list of Bazel targets that include the files from above
-buildables=$(bazel query \
-    --keep_going \
-    --noshow_progress \
-    "kind(kibot, rdeps(//..., set(${files[*]})))" 2>/dev/null)
+# # Creates a list of .kicad_pcb and .kicad_sch files that have changed since the
+# # GITHUB_BASE_REF
+# files=()
+# for file in $(find vehicle/mkv -type f | grep "kicad_pcb$\|kicad_sch$"); do
+#     files+=($(bazel query --keep_going --noshow_progress $file))
+# done
 
-# Only build if there are things that have changed
-if [[ ! -z $buildables ]]; then
-    echo "Building:"
-    echo "${buildables}"
+# # Gets a list of Bazel targets that include the files from above
+# buildables=$(bazel query \
+#     --keep_going \
+#     --noshow_progress \
+#     "kind(kibot, rdeps(//..., set(${files[*]})))" 2>/dev/null)
 
-    # Build all of the targets that have changed
-    bazelisk build \
-        --config=docker-kicad \
-        --noshow_progress \
-        --build_tag_filters=kicad \
-        $buildables 2>/dev/null
+# # Only build if there are things that have changed
+# if [[ ! -z $buildables ]]; then
+#     echo "Building:"
+#     echo "${buildables}"
 
-    # Clear out the build/ folder
-    rm -rf build
-    mkdir -p build
+#     # Build all of the targets that have changed
+#     bazelisk build \
+#         --config=docker-kicad \
+#         --noshow_progress \
+#         --build_tag_filters=kicad \
+#         $buildables 2>/dev/null
 
-    # Copy all the built files from their Bazel folder to the `build/` folder
-    site_update_data=()
-    for file in $buildables; do
-        file="${file//://}"
-        mkdir -p build/$(dirname ${file:2})
-        cp $(bazelisk info bazel-genfiles)/${file:2} build/${file:2}
-        if [ "${file: -3}" == pdf ]; then
-            site_update_data+=$file
-            layout=${file:2:-3}"kicad_pcb"
-            if test -f "$layout" && [ -s $layout ]; then
-                python3 InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py $layout --no-browser
-                parentdir="$(dirname "$layout")"
-                echo "one"
-                ls build/$parentdir
-                echo "two"
-                ls $parentdir
-                cp "$parentdir/bom/ibom.html" "build/$parentdir/ibom.html"
-                site_update_data+="//$parentdir/ibom.html"
-            else
-                site_update_data+="//none"
-            fi
-        fi
-    done
+#     # Clear out the build/ folder
+#     rm -rf build
+#     mkdir -p build
 
-    # Create a Markdown file that will be uploaded as a GitHub PR comment
-    echo "Creating GH comment"
-    echo "# KiCad Artifacts" >> build/comment.md
+#     # Copy all the built files from their Bazel folder to the `build/` folder
+#     site_update_data=()
+#     for file in $buildables; do
+#         file="${file//://}"
+#         mkdir -p build/$(dirname ${file:2})
+#         cp $(bazelisk info bazel-genfiles)/${file:2} build/${file:2}
+#         if [ "${file: -3}" == pdf ]; then
+#             site_update_data+=$file
+#             layout=${file:2:-3}"kicad_pcb"
+#             if test -f "$layout" && [ -s $layout ]; then
+#                 python3 InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py $layout --no-browser
+#                 parentdir="$(dirname "$layout")"
+#                 echo "one"
+#                 ls build/$parentdir
+#                 echo "two"
+#                 ls $parentdir
+#                 cp "$parentdir/bom/ibom.html" "build/$parentdir/ibom.html"
+#                 site_update_data+="//$parentdir/ibom.html"
+#             else
+#                 site_update_data+="//none"
+#             fi
+#         fi
+#     done
 
-    # List of files that were generated
-    for file in $(find build -type f); do
-        chmod 777 $file
-        if [[ ! $file == "build/comment.md" ]]; then
-            url="https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/${file#build/}"
-            echo "<li><a href=\"$url\">$(basename $file)</a></li>" >> build/comment.md
-        fi
-    done
+#     # Create a Markdown file that will be uploaded as a GitHub PR comment
+#     echo "Creating GH comment"
+#     echo "# KiCad Artifacts" >> build/comment.md
 
-    # By default, SVGs are have transparent backgrounds, which makes things hard
-    # to read when using dark-mode on Github. This converts them to have white
-    # backgrounds
-    echo "Converting SVGs to use white backgrounds"
-    for file in $(find build -name '*.svg' -type f); do
-        cp ${file} ${file}_old
-        rsvg-convert -b white -f svg ${file}_old > ${file}
-        rm -rf ${file}_old
-    done
+#     # List of files that were generated
+#     for file in $(find build -type f); do
+#         chmod 777 $file
+#         if [[ ! $file == "build/comment.md" ]]; then
+#             url="https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/${file#build/}"
+#             echo "<li><a href=\"$url\">$(basename $file)</a></li>" >> build/comment.md
+#         fi
+#     done
 
-    # Adds SVGs of the layouts as images (uses the same caching principles as
-    # above).
-    for file in $(find build -name "*_pcb.svg" -type f); do
-        echo "<p align=\"center\">" >> build/comment.md
-        echo "<img src=\"https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/${file#build/}?ref=${GITHUB_SHA}\" width=\"60%\"/>" >> build/comment.md
-        echo "</p>" >> build/comment.md
-    done
+#     # By default, SVGs are have transparent backgrounds, which makes things hard
+#     # to read when using dark-mode on Github. This converts them to have white
+#     # backgrounds
+#     echo "Converting SVGs to use white backgrounds"
+#     for file in $(find build -name '*.svg' -type f); do
+#         cp ${file} ${file}_old
+#         rsvg-convert -b white -f svg ${file}_old > ${file}
+#         rm -rf ${file}_old
+#     done
 
-    echo "Sending POST request to artifacts site to trigger update"
-    echo "Post Request Result:"
-    curl -X POST -H "Content-type: application/json" -d "{\"commit_hash\": \""${GITHUB_SHA}"\", \"updated_board_files\": \"${site_update_data}\"}" "https://kicad.olinelectricmotorsports.com"
+#     # Adds SVGs of the layouts as images (uses the same caching principles as
+#     # above).
+#     for file in $(find build -name "*_pcb.svg" -type f); do
+#         echo "<p align=\"center\">" >> build/comment.md
+#         echo "<img src=\"https://oem-outline.nyc3.digitaloceanspaces.com/kicad-artifacts/${file#build/}?ref=${GITHUB_SHA}\" width=\"60%\"/>" >> build/comment.md
+#         echo "</p>" >> build/comment.md
+#     done
 
-else
-    echo "Nothing to build"
-fi
+#     echo "Sending POST request to artifacts site to trigger update"
+#     echo "Post Request Result:"
+#     curl -X POST -H "Content-type: application/json" -d "{\"commit_hash\": \""${GITHUB_SHA}"\", \"updated_board_files\": \"${site_update_data}\"}" "https://kicad.olinelectricmotorsports.com"
+
+# else
+#     echo "Nothing to build"
+# fi
