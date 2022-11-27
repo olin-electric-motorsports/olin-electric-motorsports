7695c6398a0ddefd2b7f40431c65d0fe985c27fd
ayushchakra <achakraborty@olin.edu>
2022-11-27 01:17:09 -0500

    testing workaround with bazel

--- .github/git-bazel-kicad.sh
+++ .github/git-bazel-kicad.sh
@@ -51,10 +51,11 @@ if [[ ! -z $buildables ]]; then
         file="${file//://}"
         mkdir -p build/$(dirname ${file:2})
         cp $(bazelisk info bazel-genfiles)/${file:2} build/${file:2}
+        echo $(bazelisk info bazel-genfiles)/${file:2}
         if [ "${file: -3}" == pdf ]; then
             site_update_data+=$file
             site_update_data+=${file:0:-4}.html
-            python3 .github/test.py ${file:2:-4}.html
+            python3 $(bazelisk info bazel-genfiles)/${file:2:-4}.html
         fi
     done
 
