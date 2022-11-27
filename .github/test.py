bf647bc0d8e7f7c3e8d6ec213d935d159fc78eca
ayushchakra <achakraborty@olin.edu>
2022-11-27 01:05:26 -0500

    linted code and deleted ibom repo

--- .github/test.py
+++ .github/test.py
@@ -1,26 +1,29 @@
 import argparse
 import os
 
+
 def make_parser():
     parser = argparse.ArgumentParser(
         description="html template adjuster",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('file',
-                        type=str,
-                        help="html file path")
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("file", type=str, help="html file path")
     return parser
 
+
 def adjust_width(file_path):
-    with open(file_path, 'r') as ibom_html:
+    with open(file_path, "r") as ibom_html:
         data = ibom_html.readlines()
 
     for index, line in enumerate(data):
         if ".bom .value" in line:
-            data[index+1] = "  width: 100px;\n"
+            data[index + 1] = "  width: 100px;\n"
+            print(data[index + 1])
 
-            with open(file_path, 'w') as updated_ibom_html:
+            with open(file_path, "w") as updated_ibom_html:
                 updated_ibom_html.writelines(data)
             break
+
 
 if __name__ == "__main__":
     print(os.listdir())
