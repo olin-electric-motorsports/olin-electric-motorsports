018307d536f4d742dfbddae2e4b907510ceae5a5
vrhinehart <113650857+vrhinehart@users.noreply.github.com>
2022-12-02 08:37:15 -0500

    Added space to the end of the command (#193)

--- scripts/install_kicad_git_filters.sh
+++ scripts/install_kicad_git_filters.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git config --local filter.kicad_sch.clean "sed -E 's/#(PWR|FLG)[0-9]+/#\1?/'"
+git config --local filter.kicad_sch.clean "sed -E 's/#(PWR|FLG)[0-9]+/#\1?/' "
 git config --local filter.kicad_sch.smudge cat
