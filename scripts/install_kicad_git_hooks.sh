547acfe74e9068085fd6614b03917269dee04e53
Elvis Wolcott <ewolcott@olin.edu>
2023-01-04 00:46:42 -0500

    update pre-checkout to work for non-branch checkouts

--- scripts/install_kicad_git_hooks.sh
+++ scripts/install_kicad_git_hooks.sh
@@ -8,38 +8,59 @@ ROOT=$(git rev-parse --show-toplevel)
 # the pre-checkout hook will prevent checkout if the symbols haven't been commited
 cat << EOF > ${ROOT}/.git/hooks/pre-checkout
 #!/bin/bash
-echo "Running pre-checkout hook. To skip this step run SKIP_PRE_CHECKOUT=true git checkout..."
-# set PRE_CHECKOUT flag
-export PRE_CHECKOUT=true
+# parameters:
+# 1: previous HEAD
+# 2: new HEAD
+# 3: new HEAD is branch
+
+echo \$3
+
 # check for SKIP_PRE_CHECKOUT flag
 if [ ! -z \$SKIP_PRE_CHECKOUT ] && \$SKIP_PRE_CHECKOUT
 then
     exit 0
 fi
 
-# switch to branch before checkout
-git switch --quiet -
+echo "Running pre-checkout hook. To skip this step run SKIP_PRE_CHECKOUT=true git checkout..."
+# set PRE_CHECKOUT flag
+export PRE_CHECKOUT=true
+
+# switch previous HEAD
+git checkout \$1 --quiet
 # ensure any symbol changes are up to date
 if test -f ${ROOT}/parts/schematic/oem.kicad_sym; then
     rm -rf ${ROOT}/parts/schematic/oem
     bazel run //tools/symbols:convert -- library2symbols --source ${ROOT}/parts/schematic/oem.kicad_sym --out ${ROOT}/parts/schematic/oem
 fi
-# switch back to destination branch
-git switch --quiet -
+# switch back new HEAD
+if [ "\$3" -eq "1" ];
+then
+    echo "Flag set"
+    # switch to previous branch
+    git switch --quiet -
+else
+    # switch to previous commit
+    git checkout \$2 --quiet
+fi
 EOF
 
 # install post-checkout hook
 # the post-checkout hook generates a kicad_sym file from the symbols in parts/schematic/oem
 cat << EOF > ${ROOT}/.git/hooks/post-checkout
 #!/bin/bash
+# parameters:
+# 1: previous HEAD
+# 2: new HEAD
+# 3: new HEAD is branch
+
 # check for PRE_CHECKOUT flag
 if [ ! -z \$PRE_CHECKOUT ] && \$PRE_CHECKOUT
 then
     exit 0
 fi
 
-# run pre-checkout hook
-. .git/hooks/pre-checkout
+# run pre-checkout hook and pass previous and new HEAD
+. .git/hooks/pre-checkout \$1 \$2 \$3
 
 bazel run //tools/symbols:convert -- symbols2library --source ${ROOT}/parts/schematic/oem --out ${ROOT}/parts/schematic/oem.kicad_sym
 EOF
