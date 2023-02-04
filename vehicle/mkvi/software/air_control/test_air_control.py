8e95a2b52b26a303df3dc01e3275f3d5cf7c32c7
richardli03 <rli@olin.edu>
2023-02-04 15:59:10 -0500

    added software folder for mkvi testing

---
+++ vehicle/mkvi/software/air_control/test_air_control.py
@@ -0,0 +1,11 @@
+# Base/extended python imports
+import pytest
+import os
+import time
+from configparser import ConfigParser
+from can import Message
+
+# Project Imports
+from projects.hitl.software.hitl.cancontroller import CANController
+
+
