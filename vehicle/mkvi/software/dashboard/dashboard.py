fb8edc187591e127d26b1bb1f829c947744a1b0a
Amanda Chang <achang2@olin.edu>
2023-03-30 18:53:25 -0400

    Added firmware functionality, 10 millisecond delay, interrupts, LED logic, and more

--- vehicle/mkvi/software/dashboard/dashboard.py
+++ vehicle/mkvi/software/dashboard/dashboard.py
@@ -1,6 +1,5 @@
 import threading
 import time
-import yaml
 import can
 import cantools
 import RPi.GPIO as GPIO
@@ -33,7 +32,7 @@ def init_can(channel, bustype, bitrate, callback, dbc):
     )
 
     listener.start()
-    return can_bus, db, kill_flag
+    return can_bus, kill_flag
 
 
 def dashboard_listener(can_bus, callback, kill_flag):
@@ -62,10 +61,9 @@ def dashboard_callback(msg, db):
     try:
         message = db.decode_message(msg.arbitration_id, msg.data)
 
-    except Exception as e:
+    except Exception as _:
         return
 
-    STATES_DICTIONARY = {}
     for signal_name in message:
         STATES_DICTIONARY[signal_name] = message.get(signal_name)
 
@@ -85,8 +83,32 @@ PROGRAMMING_LED_1 = 40
 PROGRAMMING_LED_2 = 38
 PROGRAMMING_LED_3 = 37
 
+# Initializing the dictionary where dashboard pin readings will be stored
+dashboard_data = {}
+
+# Enabling the start button interrupt
+def button_pressed_callback():
+    STATES_DICTIONARY["start_button_state"] = True
+    dashboard_data["start_button_state"] = True
+
+
+def shutdown_callback(channel):
+    if channel == BOTS_SHDN_SENSE:
+        STATES_DICTIONARY["ss_bots"] = True
+        dashboard_data["ss_bots"] = True
+    if channel == E_STOP_SHDN_SENSE:
+        STATES_DICTIONARY["ss_estop"] = True
+        dashboard_data["ss_estop"] = True
+
 
 def main():
+    """
+    Contains the superloop intended to run every 10 milliseconds;
+    Separate thread continually updates STATES_DICTIONARY from the CAN bus
+    The values in STATES_DICTIONARY determine whether certain states/lights
+    should be activated. `dashboard_data` is a dictionary containing info
+    on the various pin readings of my board.
+    """
     # Set pin numbering system
     GPIO.setmode(GPIO.BOARD)
 
@@ -101,44 +123,95 @@ def main():
         PROGRAMMING_LED_2,
         PROGRAMMING_LED_3,
     ]
+
     channel_inputs = [RTD_BUTTON_SENSE, BOTS_SHDN_SENSE, E_STOP_SHDN_SENSE]
     GPIO.setup(channel_outputs, GPIO.OUT)
     GPIO.setup(channel_inputs, GPIO.IN)
 
     # Initialize a thread to constantly update STATES_DICTIONARY from CAN messages
-    init_can("/dev/ttyACM1", "slcan", 500000, dashboard_callback, dbc_file)
+    can_bus, kill_flag = init_can(
+        "/dev/ttyACM1", "slcan", 500000, dashboard_callback, dbc_file
+    )
+
+    # Adding start button, bots shutdown, and estop shutdown interrupts
+    GPIO.add_event_detect(
+        RTD_BUTTON_SENSE, GPIO.BOTH, callback=button_pressed_callback, bouncetime=50
+    )
+    GPIO.add_event_detect(BOTS_SHDN_SENSE, GPIO.FALLING, callback=shutdown_callback)
+    GPIO.add_event_detect(E_STOP_SHDN_SENSE, GPIO.FALLING, callback=shutdown_callback)
 
     # Number of seconds the buzzer sounds
-    seconds = 4
+    RTD_BUZZ_TIME = 2000  # in milliseconds
+    buzzer_counter = 0  # buzzer_counter starts at 0
 
-    while True:
-        # Activates ready to drive
+    dashboard = db.get_message_by_name("dashboard")
+
+    while True:  # MAKE THIS RUN EVERY 10 MILLISECONDS
+        t0 = time.perf_counter()
+
+        THROTTLE_PRESSED = (
+            STATES_DICTIONARY["throttle_l_pos"]
+            >= 12 | STATES_DICTIONARY["throttle_r_pos"]
+            >= 12
+        )
+
+        # Turns on AMS LED if there are any AMS faults
+        GPIO.output(AMS_LED_LSD, STATES_DICTIONARY["bms_fault"] != "NONE")
+
+        # Turns on HV LED if the tractive system is on
+        GPIO.output(HV_LED_LSD, STATES_DICTIONARY["air_state"] == "TS_ACTIVE")
+
+        # Turns on IMD LED if there is an IMD fault; imd_status True is good
+        GPIO.output(IMD_LED_LSD, not STATES_DICTIONARY["imd_status"])
+
+        # Turns on the button LED if brakes are pressed, tractive system is on,
+        # RTD is not on, and the throttle is not being pressed
+        if (
+            STATES_DICTIONARY["brake_gate"]
+            and STATES_DICTIONARY["air_state"] == "TS_ACTIVE"
+            and not STATES_DICTIONARY["ready_to_drive"]
+            and not THROTTLE_PRESSED
+        ):
+            GPIO.output(RTD_BUTTON_LED, 1)  # Turn on button LED
+        else:
+            GPIO.output(RTD_BUTTON_LED, 0)
+
+        # Update "ready_to_drive", turn off the button LED and start the buzzer
+        # Set the counter to 0 to start off with
         if (
             STATES_DICTIONARY["air_state"] == "TS_ACTIVE"
             and STATES_DICTIONARY["brake_gate"]
-            is False  ## Am I supposed to use bspd_current_sense?
-            and STATES_DICTIONARY["start_button_state"] is True
+            and STATES_DICTIONARY["start_button_state"]
+            and not THROTTLE_PRESSED
         ):
             STATES_DICTIONARY["ready_to_drive"] = True
-            # Sounds the buzzer for exactly seconds number of seconds
-            if STATES_DICTIONARY["ready_to_drive"] is True:
-                GPIO.output(RTD_BUTTON_LED, 1)  # Turn on RTD LED
-                GPIO.output(RTD_BUZZER_LSD, 1)  # Start buzzer
-                time.sleep(seconds)
-                GPIO.output(RTD_BUZZER_LSD, 0)  # Stop buzzer after some seconds
+            dashboard_data["ready_to_drive"] = True
+            GPIO.output(RTD_BUTTON_LED, 0)
+            GPIO.output(RTD_BUZZER_LSD, 1)
 
-        # Turns on AMS LED if there are any AMS faults
-        if STATES_DICTIONARY["bms_fault"] != "NONE":
-            GPIO.output(AMS_LED_LSD, 1)
+        # Turn off the button LED and RTD false in dicts if tractive system not active
+        # Also reset the buzzer
+        if STATES_DICTIONARY["air_state"] != "TS_ACTIVE":
+            GPIO.output(RTD_BUTTON_LED, 0)  # Turn off button LED
+            STATES_DICTIONARY["ready_to_drive"] = False  # Disable RTD in dictionary
+            dashboard_data["ready_to_drive"] = False
+            buzzer_counter = 0
 
-        # Turns on HV LED if both the air positive and air negative are closed
-        if (
-            STATES_DICTIONARY["air_p_state"]
-            and STATES_DICTIONARY["air_n_state"] is True
-        ):
-            GPIO.output(HV_LED_LSD, 1)
+        # Add 10 milliseconds to the buzzing timer every loop
+        if STATES_DICTIONARY["ready_to_drive"] and buzzer_counter < RTD_BUZZ_TIME:
+            buzzer_counter += 10
 
-        # Turns on IMD LED if there is an IMD fault
+        if buzzer_counter >= RTD_BUZZ_TIME:
+            GPIO.output(RTD_BUZZER_LSD, 0)
+
+        data = dashboard.encode(dashboard_data)
+        message = can.Message(arbitration_id=dashboard.frame_id, data=data)
+        can_bus.send(message)  # Send out RTD status over CAN
+
+        # Make the loop run every 10 milliseconds
+        delta = time.perf_counter() - t0
+        if delta < 0.01:
+            time.sleep(0.01 - delta)
 
 
 # Stores code that should only run when dashboard.py is run as a script and not
