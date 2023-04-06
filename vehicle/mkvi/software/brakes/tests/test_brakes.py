128140ef1e4191b233dd2148a358b98d1ca44ed2
Ian <iwalsh@olin.edu>
2023-04-06 18:24:45 -0400

    added test folder and .py file

---
+++ vehicle/mkvi/software/brakes/tests/test_brakes.py
@@ -0,0 +1,23 @@
+#################### TEST 1 ####################
+#Power on the 16M1
+
+#################### TEST 2 ####################
+#Sweep voltage on brake pressure input
+#should toggle brakelight + output CAN message
+
+#################### TEST 3 ####################
+#Digital On/Off of 5kW pin
+#should output CAN message
+
+#################### TEST 4 ####################
+#5kW HIGH and brake pressure signal in the 'on' range
+#Should toggle SHOUTDOWN sense pin, trip SHUTDOWN line, and output CAN message
+
+#################### TEST 5 ####################
+#5kW HIGH and brake pressure signal in the 'on' range
+#Should toggle SHOUTDOWN sense pin, trip SHUTDOWN line, and output CAN message
+
+#################### TEST 6 ####################
+#toggle breaklight high
+#test with an actual load
+
