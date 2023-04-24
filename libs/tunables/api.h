0da5f443ae2587e9f42c0facf4e0e38501847dba
tlee10333 <trinityl10333@gmail.com>
2023-04-24 01:38:24 -0400

    Made api.h file & fixed Build file

---
+++ libs/tunables/api.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 Olin Electric Motorsports
+ */
+#pragma once
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#define MAX_NUM_PARAMS (16)
+
+typedef enum { GET, SET, MEASURE } message_type_t;
+
+/*
+ * tunables_init
+ *
+ * Initializes ECU ids locally and allows us to bring EEPROM into RAM for easy
+ * reading
+ *
+ * Parameters:
+ *  - (uint16_t) ecu_id: the ECU id
+ *  - (void*) tunables_data: A pointer to the tunable parameter data
+ *  - (size_t) num_tunables: Total number of tunable parameters
+ * */
+void tunables_init(uint16_t ecu_id, void* tunables_data, size_t num_tunables);
+
+/*
+ * tunables_loop
+ *
+ * Goes into the super loop and read/writes to EEPROM if CAN msg was received
+ *
+ * Parameters:
+ *  - (void*) tunables_data: A pointer to the tunable parameter data
+ *  - (size_t) num_tunables: Total number of tunable parameters
+ */
+void tunables_loop(void* tunables_data, size_t num_tunables);
