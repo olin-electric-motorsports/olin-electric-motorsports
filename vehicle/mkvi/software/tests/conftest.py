599d900e16ad5e0b5b7a0ceb1e0898886c1c5229
Jack Greenberg <j@jackgreenberg.co>
2023-05-01 13:12:24 -0400

    Remove autoflashing for now

--- vehicle/mkvi/software/tests/conftest.py
+++ vehicle/mkvi/software/tests/conftest.py
@@ -66,16 +66,15 @@ def btldr(canbus):
     btldr.canbus = canbus
 
     # Flash
-    # for ecu in ECUS:
-    #     resp = btldr.ping(ecu["btldr_id"], 1)
-    #
-    #     if resp:
-    #         log.info("Found {}, flashing.".format(ecu["name"]))
-    #         btldr.flash(ecu["btldr_id"], ecu["binary"], 1)
-    #     else:
-    #         log.warning("Unable to ping {}. Is it connected?".format(ecu["name"]))
+    for ecu in ECUS:
+        resp = btldr.ping(ecu["btldr_id"], 1)
 
-    yield btldr
+        if resp:
+            log.info("Found {}, flashing.".format(ecu["name"]))
+        else:
+            log.warning("Unable to ping {}. Is it connected?".format(ecu["name"]))
+
+    return btldr
 
 
 @pytest.fixture(scope = "session")
