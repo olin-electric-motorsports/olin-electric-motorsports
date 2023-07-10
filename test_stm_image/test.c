5642ccc63b70b2808a91f822a02a340dd097f590
ayushchakra <achakraborty@olin.edu>
2023-07-09 22:52:47 -0400

    debugging

--- test_stm_image/test.c
+++ test_stm_image/test.c
@@ -1,8 +1,14 @@
 #include <libopencm3/stm32/common.h>
-#include <libopencm3/stm32/f1/adc.h>
+#include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/f1/memorymap.h>
+#include <libopencm3/stm32/f1/rcc.h>
+
 
 int main(void) {
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL,
+                  GPIO13);
     while (1) {
-        continue;
+        gpio_set(GPIOC, GPIO13);
     }
 }
