d77d8bd73478fdb56aee1d83fd5e18f2b2bd76b1
drewnotdrew <silverbird337@gmail.com>
2023-10-07 19:55:42 -0400

    Simplify software timer based on feedback

--- tutorials/super_blink/super_blink.c
+++ tutorials/super_blink/super_blink.c
@@ -5,27 +5,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define HW_TIMER_Hz     (30)
-#define SW_TIMER_SCALER (6)
+#define TIMER_5HZ     (5)
 
 static volatile bool run_blink_led0 = true;
-static volatile bool run_blink_led1 = true;
 uint8_t counter = 0;
 
 void timer0_callback(void) {
     run_blink_led0 = true;
-}
-
-void sw_timer_callback(void) {
-    run_blink_led1 = true;
-}
-
-void sw_timer(void) {
-    counter += 1;
-    if (counter % (HW_TIMER_Hz / SW_TIMER_SCALER) == 0) {
-        sw_timer_callback();
-        counter = 0;
-    }
 }
 
 int main(void) {
@@ -40,12 +26,11 @@ int main(void) {
         if (run_blink_led0) {
             gpio_toggle_pin(LED0);
             run_blink_led0 = false;
-            sw_timer();
+            counter++;
         }
 
-        if (run_blink_led1) {
+        if (counter % TIMER_5HZ) {
             gpio_toggle_pin(LED1);
-            run_blink_led1 = false;
         }
     }
 }
