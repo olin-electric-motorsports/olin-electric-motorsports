e9f351f6cb3fb863e8d2ecb193dcb668aab6e6d4
drewnotdrew <silverbird337@gmail.com>
2023-11-15 01:56:59 -0500

    Adjust thermistor transfer function

--- projects/canviewer/utils.py
+++ projects/canviewer/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from canviewer import PROCESSING_FUNCTIONS
 
 
-def convertVtoT(x, Vin=3, R1=10000, R2=100000, T2=348.15, beta=3988):
+def convertVtoT(x, Vin=3, R1=100000, R2=100000, T2=298.15, beta=4100): # Unsure if beta is 4100 or 4300
     """
     converts voltage drop data to temperature
     x: array containing only the voltages
@@ -10,7 +10,6 @@ def convertVtoT(x, Vin=3, R1=10000, R2=100000, T2=348.15, beta=3988):
     Vout = float(x)
     thermistor_R1 = (Vout * R1) / (Vin - Vout)
     temperature = 1 / ((np.log(thermistor_R1 / R2) / beta) + (1 / T2)) - 273.15
-
     return temperature
 
 
