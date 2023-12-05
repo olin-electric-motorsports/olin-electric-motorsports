b82ebeef50cccac95b3a5d1d008725de7c037c85
yanashley <ashley.of.yang@gmail.com>
2023-12-04 21:36:39 -0500

    added receiving can messages, moved bms charging to subscrible in charger.yml

--- vehicle/mkvi/software/charging/charger.c
+++ vehicle/mkvi/software/charging/charger.c
@@ -4,6 +4,9 @@
 #include "vehicle/mkvi/software/charging/charger.h"
 #include "vehicle/mkvi/software/charging/can_api.h"
 
+#define TARGET_PACK_VOLTAGE (360) // in volts
+#define CHARGING_MAX_VOLTAGE (201) // 3201 = 320.1V
+#define CHARGING_MAX_CURRENT (582) // 582 = 58.2A
 
 void timer0_isr(void) {
     // doing smth;
@@ -11,27 +14,40 @@ void timer0_isr(void) {
 
 //loop
 int main(void) {
+
+    can_receive_bms_core();
+    can_receive_charging_fbk();
+    can_receive_bms_charging(); // will be added into bms.yml soon
+
     while(1) {
         //check status of BMS
-        if (bms_charging.charger_connected){
-            // get_bms_voltage
-            bms_charging.charge_enable = true;
-            charging_cmd.max_voltage = 3201; // 3201 = 320.1V
-            charging_cmd.max_current = 582; // 582 = 58.2A
+        // poll like asking a question is there can data or not; verifying if the message exists or not
+        if (can_poll_receive_bms_core() == 0){
+            can_receive_bms_core(); // getting can data
         }
-        else {
-            bms_charging.charge_enable = false;
+        if (can_poll_receive_charging_fbk() == 0){
+            can_receive_charging_fbk();
         }
-
+        // values would initialize to 0 if poll receives fail
+        if (bms_core.pack_voltage < TARGET_PACK_VOLTAGE){
+            if (bms_charging.charge_enable){
+                charging_cmd.enable = true;
+                charging_cmd.max_voltage = CHARGING_MAX_VOLTAGE; 
+                charging_cmd.max_current = CHARGING_MAX_CURRENT; 
+            }
+            else {
+                charging_cmd.enable = false;
+            }
+        }
         //safety checks
         if (charging_fbk.hardware_fault) {
-            bms_charging.charge_enable = false;
+            charging_cmd.enable = false;
         }
         if (charging_fbk.temperature_protection) {
-            bms_charging.charge_enable = false;
+            charging_cmd.enable = false;
         }
         if (charging_fbk.charging_voltage > charging_cmd.max_voltage || charging_fbk.charging_current > charging_cmd.max_current) {
-            bms_charging.charge_enable = false;
+            charging_cmd.enable = false;
         }
 
     }
