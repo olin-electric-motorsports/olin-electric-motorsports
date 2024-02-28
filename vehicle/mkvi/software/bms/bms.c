8df33fc5a21f69f7c46b3cd7a76deacab879783d
Drew <silverbird337@gmail.com>
2024-02-28 09:25:50 -0500

    Pull voltage task code from amit/bms-code-dev
    Currently building, untested

--- vehicle/mkvi/software/bms/bms.c
+++ vehicle/mkvi/software/bms/bms.c
@@ -13,7 +13,7 @@
 
 #include "vehicle/mkvi/software/bms/bms_config.h"
 #include "vehicle/mkvi/software/bms/can_api.h"
-// #include "vehicle/mkvi/software/bms/tasks/tasks.h"
+#include "vehicle/mkvi/software/bms/tasks/tasks.h"
 #include "vehicle/mkvi/software/bms/utils/fault.h"
 #include "vehicle/mkvi/software/bms/utils/mux.h"
 
@@ -67,11 +67,87 @@ void hw_init() {
     wakeup_sleep(NUM_ICS);
 }
 
+static void monitor_cells(void) {
+    // Set a new fault
+    if (bms_core.bms_fault != BMS_FAULT_NONE) {
+        bms_core.bms_state = BMS_STATE_FAULT;
+        gpio_clear_pin(BMS_RELAY_LSD);
+    }
+
+    // Handle condition where fault was cleared
+    // TODO: also need to handle charging here
+    if (bms_core.bms_state == BMS_STATE_FAULT
+        && bms_core.bms_fault == BMS_FAULT_NONE) {
+        bms_core.bms_state = BMS_STATE_ACTIVE;
+    }
+
+    // read all voltages
+    uint32_t ov = 0;
+    uint32_t uv = 0;
+
+    uint16_t pack_voltage = 0;
+    int rc = voltage_task(&pack_voltage, &ov, &uv);
+    bms_core.pack_voltage = pack_voltage;
+
+    // Check for PEC errors
+    if (rc != 0) {
+        bms_metrics.voltage_pec_error_count += rc;
+
+        if (bms_metrics.voltage_pec_error_count >= MAX_PEC_ERROR_COUNT) {
+            set_fault(BMS_FAULT_PEC);
+            bms_core.bms_state = BMS_STATE_FAULT;
+        }
+        return;
+    } else {
+        bms_metrics.voltage_pec_error_count = 0;
+    }
+
+    if (ov > 0) {
+        set_fault(BMS_FAULT_OVERVOLTAGE);
+        bms_core.bms_state = BMS_STATE_FAULT;
+    }
+
+    // switch (bms_core.bms_state) {
+    //     case BMS_STATE_ACTIVE: {
+    //         if (uv > 0) {
+    //             set_fault(BMS_FAULT_UNDERVOLTAGE);
+    //             bms_core.bms_state = BMS_STATE_FAULT;
+    //         }
+    //     } break;
+    //     case BMS_STATE_CHARGING: {
+    //         if (can_poll_receive_charging_fbk() == 0) {
+    //             uint8_t charger_status = charging_fbk_data[4];
+    //             if (charger_status != 0) {
+    //                 if ((charger_status & (1 << 3)) == 0) {
+    //                 } else if ((charger_status & (1 << 2)) == 0) {
+    //                 } else {
+    //                     set_fault(BMS_FAULT_CHARGER_FAULT);
+    //                 }
+    //             }
+
+    //             can_receive_charging_fbk();
+    //         }
+    //     } break;
+    //     case BMS_STATE_FAULT: {
+    //         gpio_clear_pin(BMS_RELAY_LSD);
+    //         charging_cmd.max_voltage = 0;
+    //         charging_cmd.max_current = 0;
+    //         charging_cmd.enable = false;
+    //     } break;
+    //     default: {
+    //         // shouldn't reach here. failsafe
+    //         set_fault(BMS_FAULT_STATE_MACHINE);
+    //         bms_core.bms_state = BMS_STATE_FAULT;
+    //     } break;
+    // }
+}
+
 int main(void) {
     hw_init();
 
     while (true) {
         if (run_10ms) {
+            monitor_cells();
             can_send_bms_core();
             can_send_bms_sense();
             run_10ms = false;
