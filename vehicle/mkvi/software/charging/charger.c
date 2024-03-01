7aeda0e0be5d2e93f830ee2d9e97b6e972d2c10a
yanashley <ashley.of.yang@gmail.com>
2024-02-29 22:18:27 -0500

    parsed data from charger

--- vehicle/mkvi/software/charging/charger.c
+++ vehicle/mkvi/software/charging/charger.c
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include "MCP25625.h"
 #include "vehicle/mkvi/software/charging/can_api.h"
+#include <stdio.h>
 
 #define TARGET_PACK_VOLTAGE (360) // in volts
 #define CHARGING_MAX_VOLTAGE (201) // 3201 = 320.1V
@@ -16,6 +17,34 @@ uint8_t count = 0;
 bool RX_IDE, RX_RTR;
 uint32_t EID = 0;
 
+float charging_voltage = 0;
+float charging_current = 0;
+bool hardware_fault = false;
+bool temperature_protection = false;
+bool input_voltage = false;
+bool starting_state = false;
+bool communication_state = false;
+bool *checks[] = {&hardware_fault, &temperature_protection, &input_voltage, &starting_state, &communication_state};
+
+// helper function
+uint16_t binaryToDecimal(uint16_t binaryNumber) {
+    uint16_t decimalNumber = 0;
+    uint16_t base = 1;
+
+    while (binaryNumber > 0) {
+        // Extract the rightmost bit of the binary number
+        uint16_t bit = binaryNumber % 10;
+
+        // Add the bit multiplied by the base to the decimal number
+        decimalNumber += bit * base;
+
+        // Move to the next bit and update the base
+        binaryNumber /= 10;
+        base *= 2;
+    }
+
+    return decimalNumber;
+}
 
 void timer0_isr(void) {
     // doing smth;_
@@ -40,12 +69,18 @@ void spi_send_charger(){
 }
 
 // receiving SPI from charger
-//next task: parse data
+//next task: parse data from charger
 void spi_receive_charger(){
     if( mcp25625_msg_ready( RXB0 ) )
         {
             mcp25625_msg_read( RXB0, rx_msg, &count, &EID, &RX_IDE, &RX_RTR );
-        }
+            // assigning spi data to variables
+            charging_voltage = binaryToDecimal((uint16_t) rx_msg[0] << 8 | rx_msg[1]) * 0.1;
+            charging_current = binaryToDecimal((uint16_t) rx_msg[2] << 8 | rx_msg[3]) * 0.1;
+            for (int i = 0; i < 5: i++){
+                *checks[i] = (bool)((rx_msg[4] >> i) & 0x01);
+            }
+        }_
 }
 
 //loop
@@ -56,7 +91,7 @@ int main(void) {
     can_init_elcon_hk_j_440_10();
 
     can_receive_bms_core();
-    can_receive_charging_fbk();
+    can_receive_charging_fbk(); // replace this with spi receive
     can_receive_bms_charging(); // will be added into bms.yml soon
 
     while(1) {
