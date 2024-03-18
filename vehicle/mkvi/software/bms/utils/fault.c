404e1028d6bbad1fae2f03317161211e97df37cf
drewnotdrew <silverbird337@gmail.com>
2024-03-17 22:50:14 -0400

    Add bms fault enum for backwards compat with canviewer

--- vehicle/mkvi/software/bms/utils/fault.c
+++ vehicle/mkvi/software/bms/utils/fault.c
@@ -7,17 +7,19 @@
 void set_fault(enum bms_fault the_fault) {
     gpio_clear_pin(BMS_RELAY_LSD);
 
-    bms_core.bms_fault |= (1 << the_fault);
+    bms_core.bms_fault_code |= (1 << the_fault);
 }
 
 void clear_fault(enum bms_fault the_fault) {
-    bms_core.bms_fault &= ((1 << the_fault) ^ UINT16_MAX);
+    bms_core.bms_fault_code &= ((1 << the_fault) ^ UINT16_MAX);
 }
 
 int check_fault_state(void) {
-    if (bms_core.bms_fault == 0) {
+    if (bms_core.bms_fault_code == 0) {
+        bms_core.bms_fault = BMS_FAULT_NONE;
         return 0;
     } else {
+        bms_core.bms_fault = BMS_FAULT_FAULT;
         return 1;
     }
 }
