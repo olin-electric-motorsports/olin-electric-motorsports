57b26b509d4fe58111a423cff48b2e8337ec50fd
drewnotdrew <silverbird337@gmail.com>
2024-09-08 00:44:02 -0500

    Add working accel read

--- vehicle/mkvi/software/imu/imu.c
+++ vehicle/mkvi/software/imu/imu.c
@@ -36,24 +36,34 @@ int main(void) {
     spi_init(&imu_spi_cfg);
     // _can_print(SPCR);
 
-    // spi_cs_low();
-    // uint8_t tx_data[2] = { 0x20, 0x00 };
-    // spi_transceive(tx_data, NULL, 2);
-    // spi_cs_high();
+    uint8_t tx_data[2] = { 0x86, 0x00 };
+    uint8_t rx_data[2];
+    // _can_print(SPCR);
+    spi_cs_low();
+    spi_transceive(tx_data, rx_data, 2);
+    spi_cs_high();
+    _can_print(rx_data[0]);
+    _can_print(rx_data[1]);
+
+    spi_cs_low();
+    uint8_t tx_data_0[2] = { 0x06, 0x01 };
+    spi_transceive(tx_data_0, NULL, 2);
+    spi_cs_high();
+
+    uint8_t tx_data_1[2] = { 0x86, 0x00 };
+    uint8_t rx_data_1[2];
+    // _can_print(SPCR);
+    spi_cs_low();
+    spi_transceive(tx_data_1, rx_data_1, 2);
+    spi_cs_high();
+    _can_print(rx_data_1[0]);
+    _can_print(rx_data_1[1]);
 
     for (;;) {
         if (can_send_imu_data) {
             gpio_toggle_pin(debug_led);
             // can_print("oem", 65535, -3);
             // _can_print(0xEA);
-            uint8_t tx_data[2] = { 0x80, 0x00 };
-            uint8_t rx_data[2];
-            // _can_print(SPCR);
-            spi_cs_low();
-            spi_transceive(tx_data, rx_data, 2);
-            spi_cs_high();
-            _can_print(rx_data[0]);
-            _can_print(rx_data[1]);
             // if (rx_data == 0) {
             //     for (;;) {
             //         ;
@@ -61,6 +71,14 @@ int main(void) {
             // }
 
             // gpio_toggle_pin(debug_led);
+            uint8_t tx_data_2[2] = { 0xAE, 0x00 };
+            uint8_t rx_data_2[2];
+            // _can_print(SPCR);
+            spi_cs_low();
+            spi_transceive(tx_data_2, rx_data_2, 2);
+            spi_cs_high();
+            _can_print(rx_data_2[0]);
+            _can_print(rx_data_2[1]);
 
             can_send_imu_data = false;
         }
