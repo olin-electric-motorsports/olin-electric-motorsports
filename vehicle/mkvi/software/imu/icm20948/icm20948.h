34dca1909020e6bf18bf105e9bfff4fbaca19311
drewnotdrew <silverbird337@gmail.com>
2024-09-09 02:06:02 -0500

    Working magnetometer dev id read

--- vehicle/mkvi/software/imu/icm20948/icm20948.h
+++ vehicle/mkvi/software/imu/icm20948/icm20948.h
@@ -29,68 +29,68 @@
 #define ACCEL_CYCLE   0x05
 #define I2C_MST_CYCLE 0x06
 
-#define PWR_MGMT_1            0x06
-#define PWR_MGMT_2            0x07
-#define INT_PIN_CFG           0x0F
-#define INT_ENABLE            0x10
-#define INT_ENABLE_1          0x11
-#define INT_ENABLE_2          0x12
-#define INT_ENABLE_3          0x13
-#define I2C_MST_STATUS        0x17
-#define INT_STATUS            0x19
-#define INT_STATUS_1          0x1A
-#define INT_STATUS_2          0x1B
-#define INT_STATUS_3          0x1C
-#define DELAY_TIMEH           0x28
-#define DELAY_TIMEL           0x29
-#define ACCEL_XOUT_H          0x2D
-#define ACCEL_XOUT_L          0x2E
-#define ACCEL_YOUT_H          0x2F
-#define ACCEL_YOUT_L          0x30
-#define ACCEL_ZOUT_H          0x31
-#define ACCEL_ZOUT_L          0x32
-#define GYRO_XOUT_H           0x33
-#define GYRO_XOUT_L           0x34
-#define GYRO_YOUT_H           0x35
-#define GYRO_YOUT_L           0x36
-#define GYRO_ZOUT_H           0x37
-#define GYRO_ZOUT_L           0x38
-#define TEMP_OUT_H            0x39
-#define TEMP_OUT_L            0x3A
-#define EXT_SLV_SENSE_DATA_00 0x3B
-#define EXT_SLV_SENSE_DATA_01 0x3C
-#define EXT_SLV_SENSE_DATA_02 0x3D
-#define EXT_SLV_SENSE_DATA_03 0x3E
-#define EXT_SLV_SENSE_DATA_04 0x3F
-#define EXT_SLV_SENSE_DATA_05 0x40
-#define EXT_SLV_SENSE_DATA_06 0x41
-#define EXT_SLV_SENSE_DATA_07 0x42
-#define EXT_SLV_SENSE_DATA_08 0x43
-#define EXT_SLV_SENSE_DATA_09 0x44
-#define EXT_SLV_SENSE_DATA_10 0x45
-#define EXT_SLV_SENSE_DATA_11 0x46
-#define EXT_SLV_SENSE_DATA_12 0x47
-#define EXT_SLV_SENSE_DATA_13 0x48
-#define EXT_SLV_SENSE_DATA_14 0x49
-#define EXT_SLV_SENSE_DATA_15 0x4A
-#define EXT_SLV_SENSE_DATA_16 0x4B
-#define EXT_SLV_SENSE_DATA_17 0x4C
-#define EXT_SLV_SENSE_DATA_18 0x4D
-#define EXT_SLV_SENSE_DATA_19 0x4E
-#define EXT_SLV_SENSE_DATA_20 0x4F
-#define EXT_SLV_SENSE_DATA_21 0x50
-#define EXT_SLV_SENSE_DATA_22 0x51
-#define EXT_SLV_SENSE_DATA_23 0x52
-#define FIFO_EN_1             0x66
-#define FIFO_EN_2             0x67
-#define FIFO_RST              0x68
-#define FIFO_MODE             0x69
-#define FIFO_COUNTH           0x70
-#define FIFO_COUNTL           0x71
-#define FIFO_R_W              0x72
-#define DATA_RDY_STATUS       0x74
-#define FIFO_CFG              0x76
-#define REG_BANK_SEL          0x7F
+#define PWR_MGMT_1           0x06
+#define PWR_MGMT_2           0x07
+#define INT_PIN_CFG          0x0F
+#define INT_ENABLE           0x10
+#define INT_ENABLE_1         0x11
+#define INT_ENABLE_2         0x12
+#define INT_ENABLE_3         0x13
+#define I2C_MST_STATUS       0x17
+#define INT_STATUS           0x19
+#define INT_STATUS_1         0x1A
+#define INT_STATUS_2         0x1B
+#define INT_STATUS_3         0x1C
+#define DELAY_TIMEH          0x28
+#define DELAY_TIMEL          0x29
+#define ACCEL_XOUT_H         0x2D
+#define ACCEL_XOUT_L         0x2E
+#define ACCEL_YOUT_H         0x2F
+#define ACCEL_YOUT_L         0x30
+#define ACCEL_ZOUT_H         0x31
+#define ACCEL_ZOUT_L         0x32
+#define GYRO_XOUT_H          0x33
+#define GYRO_XOUT_L          0x34
+#define GYRO_YOUT_H          0x35
+#define GYRO_YOUT_L          0x36
+#define GYRO_ZOUT_H          0x37
+#define GYRO_ZOUT_L          0x38
+#define TEMP_OUT_H           0x39
+#define TEMP_OUT_L           0x3A
+#define EXT_SLV_SENS_DATA_00 0x3B
+#define EXT_SLV_SENS_DATA_01 0x3C
+#define EXT_SLV_SENS_DATA_02 0x3D
+#define EXT_SLV_SENS_DATA_03 0x3E
+#define EXT_SLV_SENS_DATA_04 0x3F
+#define EXT_SLV_SENS_DATA_05 0x40
+#define EXT_SLV_SENS_DATA_06 0x41
+#define EXT_SLV_SENS_DATA_07 0x42
+#define EXT_SLV_SENS_DATA_08 0x43
+#define EXT_SLV_SENS_DATA_09 0x44
+#define EXT_SLV_SENS_DATA_10 0x45
+#define EXT_SLV_SENS_DATA_11 0x46
+#define EXT_SLV_SENS_DATA_12 0x47
+#define EXT_SLV_SENS_DATA_13 0x48
+#define EXT_SLV_SENS_DATA_14 0x49
+#define EXT_SLV_SENS_DATA_15 0x4A
+#define EXT_SLV_SENS_DATA_16 0x4B
+#define EXT_SLV_SENS_DATA_17 0x4C
+#define EXT_SLV_SENS_DATA_18 0x4D
+#define EXT_SLV_SENS_DATA_19 0x4E
+#define EXT_SLV_SENS_DATA_20 0x4F
+#define EXT_SLV_SENS_DATA_21 0x50
+#define EXT_SLV_SENS_DATA_22 0x51
+#define EXT_SLV_SENS_DATA_23 0x52
+#define FIFO_EN_1            0x66
+#define FIFO_EN_2            0x67
+#define FIFO_RST             0x68
+#define FIFO_MODE            0x69
+#define FIFO_COUNTH          0x70
+#define FIFO_COUNTL          0x71
+#define FIFO_R_W             0x72
+#define DATA_RDY_STATUS      0x74
+#define FIFO_CFG             0x76
+#define REG_BANK_SEL         0x7F
 
 // Bank 1
 #define SELF_TEST_X_GYRO        0x02
@@ -157,10 +157,52 @@
 #define I2C_SLV4_DI        0x17
 #define REG_BANK_SEL       0x7F
 
+// Magnetometer I2C address
+#define MAGNETOMETER_ADDR 0x0C
+
 /**
  * Magnetometer register map
  */
-// TODO
+#define WIA2  0x01
+#define ST1   0x10
+#define HXL   0x11
+#define HXH   0x12
+#define HYL   0x13
+#define HYH   0x14
+#define HZL   0x15
+#define HZH   0x16
+#define ST2   0x18
+#define CNTL2 0x31
+#define CNTL3 0x32
+#define TS1   0x33
+#define TS2   0x34
+
+/**
+ * Bit for setting read/write status in I2C address.
+ */
+#define I2C_READ  1
+#define I2C_WRITE 0
+
+/**
+ * Initialize the internal I2C controlled magnetometer in the ICM.
+ */
+void init_magnetometer(void);
+
+/**
+ * Read a register in the magnetometer.
+ *
+ * @param register_ (uint8_t) The register to read.
+ * @param rx_data (uint8_t*) A pointer to store the read data.
+ */
+void read_magnetometer(uint8_t register_, uint8_t* rx_data);
+
+/**
+ * Write a register in the magnetometer.
+ *
+ * @param register_ (uint8_t) The register to write.
+ * @param tx_data (uint8_t) The data to write.
+ */
+void write_magnetometer(uint8_t _register, uint8_t tx_data);
 
 /**
  * Read data from an ICM register.
--- vehicle/mkvi/software/imu/imu.c
+++ vehicle/mkvi/software/imu/imu.c
@@ -93,15 +93,21 @@ void init_imu(void) {
     // Wake from sleep
     icm_write_register(PWR_MGMT_1, 0x01);
 
+    // //
+    // icm_write_register(USER_CTRL, 0x20);
+
     switch_register_bank(BANK_2);
 
     // Set accelerometer full scale range
-    icm_write_register(ACCEL_CONFIG, 0x1 + (accel_fsr << 1));
+    icm_write_register(ACCEL_CONFIG, 0x1 | (accel_fsr << 1));
 
     // Set gyroscope full scale range
-    icm_write_register(GYRO_CONFIG_1, 0x01 + (gyro_fsr << 1));
+    icm_write_register(GYRO_CONFIG_1, 0x01 | (gyro_fsr << 1));
 
     switch_register_bank(BANK_0);
+
+    // Initialize magnetometer
+    init_magnetometer();
 }
 
 /**
@@ -111,13 +117,13 @@ void read_accel_data(void) {
     uint8_t accel_data[2] = { 0x0, 0x0 };
     icm_read_register(ACCEL_XOUT_L, &accel_data[0]);
     icm_read_register(ACCEL_XOUT_H, &accel_data[1]);
-    imu_accel.x = accel_data[0] + (accel_data[1] << 8);
+    imu_accel.x = accel_data[0] | (accel_data[1] << 8);
     icm_read_register(ACCEL_YOUT_L, &accel_data[0]);
     icm_read_register(ACCEL_YOUT_H, &accel_data[1]);
-    imu_accel.y = accel_data[0] + (accel_data[1] << 8);
+    imu_accel.y = accel_data[0] | (accel_data[1] << 8);
     icm_read_register(ACCEL_ZOUT_L, &accel_data[0]);
     icm_read_register(ACCEL_ZOUT_H, &accel_data[1]);
-    imu_accel.z = accel_data[0] + (accel_data[1] << 8);
+    imu_accel.z = accel_data[0] | (accel_data[1] << 8);
 }
 
 /**
@@ -127,13 +133,13 @@ void read_gyro_data(void) {
     uint8_t gyro_data[2] = { 0x0, 0x0 };
     icm_read_register(GYRO_XOUT_L, &gyro_data[0]);
     icm_read_register(GYRO_XOUT_H, &gyro_data[1]);
-    imu_gyro.x = gyro_data[0] + (gyro_data[1] << 8);
+    imu_gyro.x = gyro_data[0] | (gyro_data[1] << 8);
     icm_read_register(GYRO_YOUT_L, &gyro_data[0]);
     icm_read_register(GYRO_YOUT_H, &gyro_data[1]);
-    imu_gyro.y = gyro_data[0] + (gyro_data[1] << 8);
+    imu_gyro.y = gyro_data[0] | (gyro_data[1] << 8);
     icm_read_register(GYRO_ZOUT_L, &gyro_data[0]);
     icm_read_register(GYRO_ZOUT_H, &gyro_data[1]);
-    imu_gyro.z = gyro_data[0] + (gyro_data[1] << 8);
+    imu_gyro.z = gyro_data[0] | (gyro_data[1] << 8);
 }
 
 int main(void) {
@@ -144,6 +150,11 @@ int main(void) {
 
     init_imu();
     // uint8_t rx_data;
+    uint8_t rx_data_2;
+
+    read_magnetometer(WIA2, &rx_data_2);
+    // _delay_ms(100);
+    // _can_print(rx_data_2);
 
     for (;;) {
         if (led_heartbeat) {
@@ -151,11 +162,17 @@ int main(void) {
             led_heartbeat = false;
         };
         if (can_send_imu_data) {
-            read_accel_data();
-            can_send_imu_accel();
+            // _delay_ms(1);
+            // switch_register_bank(BANK_3);
+            // icm_read_register(I2C_SLV4_REG, &rx_data);
+            // _can_print(rx_data);
 
-            read_gyro_data();
-            can_send_imu_gyro();
+            // switch_register_bank(BANK_0);
+            // read_accel_data();
+            // can_send_imu_accel();
+
+            // read_gyro_data();
+            // can_send_imu_gyro();
             can_send_imu_data = false;
         }
     }
