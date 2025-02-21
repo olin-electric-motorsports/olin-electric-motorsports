7007e0c12a477ee5467c3ddd390febdb5a832061
KiwiAviation <jacobkeller737@gmail.com>
2025-02-21 00:02:20 -0500

    Starting code for display driver test

--- examples/16M1/display_test/display_driver.c
+++ examples/16M1/display_test/display_driver.c
@@ -1,15 +1,40 @@
-#include <util/delay.h>
+#include "examples/16M1/display_test/display_driver.h"
 
-#include "libs/gpio/api.h"
-#include "libs/gpio/pin_defs.h"
+// Initialize SPI communication with display driver
+void spi_bus_init(){
+    spi_init(&spi_cfg);
+}
 
-gpio_t LED0 = PD5;
+// Write to display driver register
+void max7221_write(uint8_t address, uint8_t data){
+    gpio_clear_pin(MAX7221_CS);  // Activate chip select
+    spi_transmit(&address, LENGTH_ADDRESS_SPI);
+    spi_transmit(&data, LENGTH_DATA_SPI);
+    gpio_set_pin(MAX7221_CS);   // Deactivate chip select
+}
+
+// Initialize display driver
+void max7221_init() {
+    gpio_set_mode(MAX7221_CS, OUTPUT);
+    gpio_set_pin(MAX7221_CS);
+
+    // Enable decoding on digits 3-0
+    max7221_write(DECODE, DECODE_4_DIGITS);
+
+    // Set scan limit to display digits 3-0
+    max7221_write(SCAN_LIMIT, SCAN_4_DIGITS);
+
+    // Set intensity to max brightness
+    max7221_write(INTENSITY, SET_MAX_BRIGHTNESS);
+
+    // Leave shutdown
+    max7221_write(SHUTDOWN, SHUTDOWN_OFF);
+
+    // Illuminate all segments
+    max7221_write(DISPLAY_TEST, DISPLAY_TEST_ON);
+}
 
 int main(void) {
-    gpio_set_mode(LED0, OUTPUT);
-
-    for (;;) {
-        gpio_toggle_pin(LED0);
-        _delay_ms(500);
-    }
+    spi_bus_init();
+    max7221_init();
 }
---
+++ examples/16M1/display_test/display_driver.h
@@ -0,0 +1,33 @@
+#include "libs/gpio/api.h"
+#include "libs/gpio/pin_defs.h"
+#include "libs/spi/api.h"
+
+#define LENGTH_ADDRESS_SPI 4
+#define LENGTH_DATA_SPI 8
+
+#define DECODE_4_DIGITS 0x0F
+#define SCAN_4_DIGITS 0x03
+#define SET_MAX_BRIGHTNESS 0x0F
+#define SHUTDOWN_OFF 0x01
+#define DISPLAY_TEST_ON 0x01
+
+enum max7221_address {
+    DECODE = 0x09,
+    INTENSITY = 0x0A,
+    SCAN_LIMIT = 0x0B,
+    SHUTDOWN = 0x0C,
+    DISPLAY_TEST = 0x0F,
+};
+
+// GPIO pin of atmega connected to chip select on display driver
+gpio_t MAX7221_CS = PB6; 
+
+spi_cfg_s spi_cfg = {
+    .interrupt_enable = false,
+    .data_order = MSB,
+    .mode = MAIN,
+    .polarity = FALLING_RISING,
+    .phase = SETUP_SAMPLE,
+    .clock_rate = F_OSC_DIV_4,
+    .cs_pin = &MAX7221_CS,
+};
