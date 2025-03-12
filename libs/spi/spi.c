449f323b6732f31afb0c182b324a8a2dc3ba3f80
KiwiAviation <jacobkeller737@gmail.com>
2025-03-12 15:12:53 -0400

    Formatting

--- libs/spi/spi.c
+++ libs/spi/spi.c
@@ -99,11 +99,11 @@ void spi_transceive_cs(uint8_t* txdata, uint8_t* rxdata, uint8_t len) {
 // Custom function for using SPI with multiple peripherals
 void spi_transceive_custom_cs(gpio_t cs, uint8_t* txdata, uint8_t* rxdata,
     uint8_t len) {
-gpio_clear_pin(cs);
-for (uint8_t i = 0; i < len; i++) {
-spi_transceive_private(txdata[i], &rxdata[i]);
-}
-gpio_set_pin(cs);
+    gpio_clear_pin(cs);
+    for (uint8_t i = 0; i < len; i++) {
+        spi_transceive_private(txdata[i], &rxdata[i]);
+    }
+    gpio_set_pin(cs);
 
 // gpio_t ADC_CS = PC1;
 // gpio_set_mode(ADC_CS, OUTPUT);
