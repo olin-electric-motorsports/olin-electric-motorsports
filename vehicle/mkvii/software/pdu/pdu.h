cbceada2106b08518f363f8fb891339aade1027b
KiwiAviation <jacobkeller737@gmail.com>
2025-03-12 15:14:04 -0400

    Create init functions for hardware

--- vehicle/mkvii/software/pdu/pdu.h
+++ vehicle/mkvii/software/pdu/pdu.h
@@ -2,6 +2,52 @@
 #include "libs/gpio/pin_defs.h"
 #include "libs/spi/api.h"
 
+// Addresses for mcp23s17 (Assumes IOCON.BANK = 0)
+enum mcp23s17_address {
+    IO_DIRECTION_A = 0x00,
+    IO_DIRECTION_B = 0x01,
+};
+
+// Commands for mcp23s17
+enum mcp23s17_command {
+    ALL_OUTPUT = 0x00,
+};
+
+// Addresses for adc1283
+enum adc1283_address {
+    ADC_CONTROL = 0x00, // Could not find address in datasheet, assuming it is 0x00
+};
+
+// Commands for adc1283
+enum adc1283_command {
+    INPUT_0 = 0x00,
+    INPUT_1 = 0x08,
+    INPUT_2 = 0x10,
+    INPUT_3 = 0x18,
+    INPUT_4 = 0x20,
+    INPUT_5 = 0x28,
+    INPUT_6 = 0x30,
+    INPUT_7 = 0x38,
+};
+
+// Addresses for max7211
+enum max7221_address {
+    DECODE = 0x09,
+    INTENSITY = 0x0A,
+    SCAN_LIMIT = 0x0B,
+    SHUTDOWN = 0x0C,
+    DISPLAY_TEST = 0x0F,
+};
+
+// Commands for max7221
+enum max7221_command{
+    DECODE_4_DIGITS = 0x0F,
+    SCAN_4_DIGITS = 0x03,
+    SET_MAX_BRIGHTNESS = 0x0F,
+    SHUTDOWN_OFF = 0x01,
+    DISPLAY_TEST_ON = 0x01,
+};
+
 // Chip Select Pins
 gpio_t MCP23S17_CS = PB3; 
 gpio_t ADC1283_CS = PB2; 
