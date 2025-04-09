c79d8782f3f5355797303016b7e9fd099f08cab1
KiwiAviation <jacobkeller737@gmail.com>
2025-04-09 19:50:43 -0400

    Debug attempt

--- vehicle/mkvii/software/pdu/pdu.c
+++ vehicle/mkvii/software/pdu/pdu.c
@@ -3,20 +3,47 @@
 // Initialize IO expander
 void mcp23S17_init() {
     // Set all GPIO pins' direction to output
-    spi_transceive_custom_cs(MCP23S17_CS, (uint8_t[]){IO_DIRECTION_A, ALL_OUTPUT}, NULL, 2);
-    spi_transceive_custom_cs(MCP23S17_CS, (uint8_t[]){IO_DIRECTION_B, ALL_OUTPUT}, NULL, 2);
+    uint8_t txdata[2] = {IO_DIRECTION_A, ALL_OUTPUT};
+    uint8_t rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 2);
+
+    txdata[0] = IO_DIRECTION_B;
+    txdata[1] = ALL_OUTPUT;
+    rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 2);
 }
 
 // Initialize display driver
 void max7221_init() {
     // Leave shutdown mode
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){SHUTDOWN, SHUTDOWN_OFF}, NULL, 2);
+    uint8_t txdata[2] = {SHUTDOWN, SHUTDOWN_OFF};
+    uint8_t rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+
     // Enable decoding on digits 3-0
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){DECODE, DECODE_4_DIGITS}, NULL, 2);
+    txdata[0] = DECODE;
+    txdata[1] = DECODE_4_DIGITS;
+    rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+    
     // Set scan limit to display digits 3-0
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){SCAN_LIMIT, SCAN_4_DIGITS}, NULL, 2);
+    txdata[0] = SCAN_LIMIT;
+    txdata[1] = SCAN_4_DIGITS;
+    rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+
     // Set intensity to max brightness
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){INTENSITY, SET_MAX_BRIGHTNESS}, NULL, 2);
+    txdata[0] = INTENSITY;
+    txdata[1] = SET_MAX_BRIGHTNESS;
+    rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+
+    // Display test
+    txdata[0] = DISPLAY_TEST;
+    txdata[1] = DISPLAY_TEST_ON;
+    rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+
 }
 
 // ADC Read
@@ -36,32 +63,39 @@ void hw_init() {
     // Initialize SPI bus
     spi_init(&spi_cfg);
     // Configure chip select pins as OUTPUT and set to high (disabled)
-    gpio_set_mode(MCP23S17_CS, OUTPUT);
-    gpio_set_pin(MCP23S17_CS);
-    gpio_set_mode(ADC1283_CS, OUTPUT);
-    gpio_set_pin(ADC1283_CS);
+    // gpio_set_mode(MCP23S17_CS, OUTPUT);
+    // gpio_set_pin(MCP23S17_CS);
+    // gpio_set_mode(ADC1283_CS, OUTPUT);
+    // gpio_set_pin(ADC1283_CS);
     gpio_set_mode(MAX7221_CS, OUTPUT);
     gpio_set_pin(MAX7221_CS);
 
     // Initialize IO expander
-    mcp23S17_init();
+    // mcp23S17_init();
 
     // Initialize display driver
     max7221_init();
 
     // Initialize CAN
-    can_init_pdu();
+    // can_init_pdu();
 }
-
 
 // Test firmware
 void hw_test(){
     // Illuminate display
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){DISPLAY_TEST, DISPLAY_TEST_ON}, (uint8_t[]){0, 0}, 2);
+    uint8_t txdata[2] = {DISPLAY_TEST, DISPLAY_TEST_ON};
+    uint8_t rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
 
     // Light up all shutdown LEDs
-    spi_transceive_custom_cs(MCP23S17_CS, (uint8_t[]){IO_GPIO_A, 0xff}, (uint8_t[]){0, 0}, 2);
-    spi_transceive_custom_cs(MCP23S17_CS, (uint8_t[]){IO_GPIO_B, 0xff}, (uint8_t[]){0, 0}, 2);
+    txdata[0] = IO_GPIO_A;
+    txdata[1] = 0xff;
+    rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+    txdata[0] = IO_GPIO_B;
+    txdata[1] = 0xff;
+    rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
 
     // Perform current reading of ADC input 1 (service section)
     uint16_t reading = adc_read(INPUT_1);
@@ -73,7 +107,7 @@ void hw_test(){
 
 int main(void) {
     hw_init();
-    hw_test();
+    //hw_test();
 
     //// Main Loop: 
     //  - Update shutdown node LEDs
