597086abcadb7242324dd396ce3dcdca2edbbe1e
KiwiAviation <jacobkeller737@gmail.com>
2025-05-08 14:12:14 -0400

    Testing current sensing

--- vehicle/mkvii/software/pdu/pdu.c
+++ vehicle/mkvii/software/pdu/pdu.c
@@ -2,10 +2,10 @@
 #include <util/delay.h>
 
 // Timer 0 setup for main loop
-volatile bool run_1ms = false; // Set as volatile to avoid compiler optimization breaking code
+volatile bool run_10ms = false; // Set as volatile to avoid compiler optimization breaking code
 void timer_0_isr(void) {
     // The following code runs every 1ms
-    run_1ms = true;
+    run_10ms = true;
 }
 
 // Timer 1 setup for LED heartbeat
@@ -16,19 +16,18 @@ void timer_1_isr(void) {
 
 // Initialize IO expander
 void mcp23S17_init() {
-    // Clear reset pin
-    gpio_set_mode(MCP23S17_RST, OUTPUT);
+    // Set reset pin
     gpio_set_pin(MCP23S17_RST);
 
     // Set all GPIO pins' direction to output
-    uint8_t tx_io[3] = {OP_WRITE, IO_DIRECTION_A, ALL_OUTPUT};
-    uint8_t rx_io = 0;
-    spi_transceive_custom_cs(MCP23S17_CS, tx_io, &rx_io, 3);
-    tx_io[0] = OP_WRITE;
-    tx_io[1] = IO_DIRECTION_B;
-    tx_io[2] = ALL_OUTPUT;
-    rx_io = 0;
-    spi_transceive_custom_cs(MCP23S17_CS, tx_io, &rx_io, 3);
+    uint8_t txdata[3] = {OP_WRITE, IO_DIRECTION_A, ALL_OUTPUT};
+    uint8_t rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 3);
+    txdata[0] = OP_WRITE;
+    txdata[1] = IO_DIRECTION_B;
+    txdata[2] = ALL_OUTPUT;
+    rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 3);
 }
 
 // Initialize display driver
@@ -107,7 +106,7 @@ void hw_init() {
     // Initialize display driver
     max7221_init();
 
-    // Initialize main loop timer
+    // Initialize timers
     sei();
     timer_init(&timer_0_cfg); // Initialize main loop timer
     timer_init(&timer_1_cfg); // Initialize heartbeat timer
@@ -121,14 +120,14 @@ void hw_test(){
     // spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
 
     // Light up all shutdown LEDs
-    uint8_t tx_io[3] = {OP_WRITE, IO_LATCH_A, 0xff};
-    uint8_t rx_io = 0;
-    spi_transceive_custom_cs(MCP23S17_CS, tx_io, &rx_io, 3);
-    tx_io[0] = OP_WRITE;
-    tx_io[1] = IO_LATCH_B;
-    tx_io[2] = 0xff;
-    rx_io = 0;
-    spi_transceive_custom_cs(MCP23S17_CS, tx_io, &rx_io, 3);
+    uint8_t txdata[3] = {OP_WRITE, IO_LATCH_A, 0xff};
+    uint8_t rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 3);
+    txdata[0] = OP_WRITE;
+    txdata[1] = IO_LATCH_B;
+    txdata[2] = 0xff;
+    rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 3);
 
     // Perform current reading of ADC input 1 (service section)
     // uint16_t reading = adc_read(INPUT_1);
@@ -160,17 +159,47 @@ void update_ts_status(){
         gpio_set_pin(TS_STATUS_G); // Turn on green light
     }
 }
-
 // ADC Read
-uint16_t adc_read(adc1283_command input_pin){
+uint16_t adc_read(uint8_t input_pin){
     // 32 clock cycles (4 bytes) are needed to read current on specified channel
-    uint8_t tx_data[] = {input_pin, 0x00, input_pin, 0x00};
+    uint8_t txdata[] = {input_pin, 0x00, input_pin, 0x00};
     // Receive 4 bytes of data, but we only care about the last 12 bits
-    uint8_t rx_data[4];
-    spi_transceive_custom_cs(ADC1283_CS, tx_data, rx_data, 4);
-    // Return the last 16 bits (first 4 bits are zero, so returning last 12 bits in effect)
-    uint16_t reading = (rx_data[2] << 8) | rx_data[3];
+    uint8_t rxdata[4];
+    spi_transceive_custom_cs(ADC1283_CS, txdata, rxdata, 4);
+    // Return the last 16 bits (only 12 bits contain data)
+    uint16_t reading = (rxdata[2] << 8) | rxdata[3];
+    // uint32_t reading = (rxdata[0] << 24) | (rxdata[1] << 16) | (rxdata[2] << 8) | rxdata[3];
     return reading;
+}
+
+// Transmit current readings over CAN
+void transmit_currents() {
+    // List of all ADC inputs for current sensors on fuses
+    uint8_t adc_inputs[6] = {INPUT_0, INPUT_1, INPUT_2, INPUT_3, INPUT_6, INPUT_7};
+    uint8_t current_readings[6];
+
+    for (int i = 0; i < 6; i++){ // Repeat for all 6 inputs
+        uint16_t adc_reading = adc_read(adc_inputs[i]); // Read ADC
+
+        int16_t delta = (int16_t)adc_reading - QVO_COUNTS; // signed difference
+    
+        // Multiply by 10 before dividing to keep precision
+        int16_t current_x10 = (delta * 10) / ACS_SENS_COUNTS; 
+    
+        if (current_x10 < 0) { current_x10 = 0; } // Clip to >0
+        current_readings[i] = (uint8_t) MIN(current_x10, 255); // Clip to <255
+    }
+
+    // Assign readings to CAN message
+    pdu_currents.cooling_x10 = current_readings[0]; // Input 0
+    pdu_currents.service_section_x10 = current_readings[1]; // Input 1
+    pdu_currents.motor_controller_x10 = current_readings[2]; // Input 2
+    pdu_currents.lv_mobo_x10 = current_readings[3]; // Input 3
+    pdu_currents.shutdown_x10 = current_readings[4]; // Input 6
+    pdu_currents.pdu_x10 = current_readings[5]; // Input 7
+
+    // Send out current readings over CAN
+    can_send_pdu_currents();
 }
 
 int main(void) {
@@ -184,24 +213,22 @@ int main(void) {
             gpio_toggle_pin(HB_LED);
             led_heartbeat = false;
         }
-        if (run_1ms) { // Run every 1ms
+        if (run_10ms) { // Run every 10ms
             // Update tractive system status LED
             update_ts_status();
 
             // Update shutdown node LEDs
 
             // Get current readings from ADC (+ publish to CAN)
-            // uint16_t reading = 0xffff;
-            // pdu_test.input1_current = reading;
-
-            // // Send CAN message
-            // can_send_pdu_test();
+            // transmit_currents();
+            pdu_test.pdu_adc_raw = adc_read(INPUT_7);
+            can_send_pdu_test();
 
             // Update display
 
             // Cooling logic
 
-            run_1ms = false; // Set run flag to false
+            run_10ms = false; // Set run flag to false
         }
     }
 }
--- vehicle/mkvii/software/pdu/pdu.h
+++ vehicle/mkvii/software/pdu/pdu.h
@@ -5,6 +5,27 @@
 #include "libs/timer/api.h"
 #include "vehicle/mkvii/software/pdu/can_api.h"
 
+// Macro that returns the minimum between two values
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+/*
+Transfer function constants for ACS37010LLZATR-030B5 current sensor and ADC1283 ADC.
+
+The ADC outputs raw digital values from 0–4095 (12-bit ADC). 
+Thus, we must convert voltage values into equivalent "ADC counts."
+
+ADC counts per volt for a 5V, 12-bit ADC:
+    4095 / 5V = 819 (ADC counts)/V
+
+For V_qvo = 2.5V (zero-current output voltage):
+    2.5V × 819 counts/V = 2047 ADC counts
+
+For ACS37010 sensitivity = 0.0667V/A:
+    0.0667 V/A × 819 counts/V ≈ 55 (ADC counts)/A
+*/
+#define QVO_COUNTS 2047 // V_qvo in ADC counts
+#define ACS_SENS_COUNTS 55 // ACS sensitivity in (ADC counts) / A
+
 // Pin definitions
 gpio_t MCP23S17_CS = PB3; // mcp23s17 - Chip select
 gpio_t ADC1283_CS = PB2; // adc1283 - Chip select
@@ -43,14 +64,14 @@ typedef enum {
 
 // Commands for adc1283
 typedef enum {
-    INPUT_0 = 0x00,
-    INPUT_1 = 0x08,
-    INPUT_2 = 0x10,
-    INPUT_3 = 0x18,
-    INPUT_4 = 0x20,
-    INPUT_5 = 0x28,
-    INPUT_6 = 0x30,
-    INPUT_7 = 0x38,
+    INPUT_0 = 0x00, // Current at cooling fuse
+    INPUT_1 = 0x08, // Current at service section fuse
+    INPUT_2 = 0x10, // Current at motor controller fuse
+    INPUT_3 = 0x18, // Current at LV motherboard fuse
+    INPUT_4 = 0x20, // Voltage at main fuse
+    INPUT_5 = 0x28, // Temperature signal from cooling
+    INPUT_6 = 0x30, // Current at shutdown fuse (roll hoop)
+    INPUT_7 = 0x38, // Current at PDU fuse
 } adc1283_command;
 
 // Addresses for max7211
@@ -71,7 +92,7 @@ typedef enum {
     DISPLAY_TEST_ON = 0x01,
 } max7221_command;
 
-// Main loop timer config
+// 10ms main loop timer config
 void timer_0_isr(void);
 timer_cfg_s timer_0_cfg = {
     .timer = TIMER0,
@@ -79,7 +100,7 @@ timer_cfg_s timer_0_cfg = {
     .prescalar = CLKIO_DIV_1024,
     .channel_a = {
         .channel = CHANNEL_A,
-        .output_compare_match = 0x12, // 217Hz
+        .output_compare_match = 0x27,
         .pin_behavior = DISCONNECTED,
         .interrupt_enable = true,
         .interrupt_callback = timer_0_isr,
