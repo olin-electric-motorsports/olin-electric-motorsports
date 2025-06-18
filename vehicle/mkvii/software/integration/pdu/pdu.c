6ec491bb347159d5fad4034d921823981330f697
H-TejadaDeras <htejada@olin.edu>
2025-06-18 08:15:05 -0400

    Added Jacob Keller's Latest PDU Firmware Version

--- vehicle/mkvii/software/integration/pdu/pdu.c
+++ vehicle/mkvii/software/integration/pdu/pdu.c
@@ -1,10 +1,68 @@
 #include "vehicle/mkvii/software/pdu/pdu.h"
+#include <util/delay.h>
+
+// Timer 0 setup for main loop
+volatile bool run_10ms = false; // Set as volatile to avoid compiler optimization breaking code
+void timer_0_isr(void) {
+    // The following code runs every 1ms
+    run_10ms = true;
+}
+
+// Timer 1 setup for software PWM
+void timer_1_isr(void) {
+    // Toggle pin to simulate 50% duty cycle
+    
+    // Commented to disable pump. Only uncomment once fluid is in cooling loop.
+    //gpio_toggle_pin(PUMP_PWM);
+}
+
+
+// Initialize IO expander
+void mcp23S17_init() {
+    // Set reset pin
+    gpio_set_pin(MCP23S17_RST);
+
+    // Set all GPIO pins' direction to output
+    uint8_t txdata[3] = {OP_WRITE, IO_DIRECTION_A, ALL_OUTPUT};
+    uint8_t rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 3);
+    txdata[0] = OP_WRITE;
+    txdata[1] = IO_DIRECTION_B;
+    txdata[2] = ALL_OUTPUT;
+    rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 3);
+}
+
+// Initialize display driver
+void max7221_init() {
+    // Leave shutdown mode
+    uint8_t txdata[2] = {SHUTDOWN, SHUTDOWN_OFF};
+    uint8_t rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+
+    // Enable decoding on digits 3-0
+    txdata[0] = DECODE;
+    txdata[1] = DECODE_4_DIGITS;
+    rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+    
+    // Set scan limit to display digits 3-0
+    txdata[0] = SCAN_LIMIT;
+    txdata[1] = SCAN_4_DIGITS;
+    rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+
+    // Set intensity to max brightness
+    txdata[0] = INTENSITY;
+    txdata[1] = SET_MAX_BRIGHTNESS;
+    rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+
+}
 
 // Initialize hardware
 void hw_init() {
-    // Initialize SPI bus
-    spi_init(&spi_cfg);
-    // Set chip select pins as OUTPUT and set to high (disabled)
+    // Configure chip select pins as OUTPUT and set to high (disable for SPI)
     gpio_set_mode(MCP23S17_CS, OUTPUT);
     gpio_set_pin(MCP23S17_CS);
     gpio_set_mode(ADC1283_CS, OUTPUT);
@@ -12,45 +70,185 @@ void hw_init() {
     gpio_set_mode(MAX7221_CS, OUTPUT);
     gpio_set_pin(MAX7221_CS);
 
+    // Configure other output GPIO pins and set to low
+    gpio_set_mode(MCP23S17_RST, OUTPUT);
+    gpio_clear_pin(MCP23S17_RST);
+    gpio_set_mode(TS_STATUS_G, OUTPUT);
+    gpio_clear_pin(TS_STATUS_G);
+    gpio_set_mode(TS_STATUS_R, OUTPUT);
+    gpio_clear_pin(TS_STATUS_R);
+    gpio_set_mode(FAN_PWM, OUTPUT);
+    gpio_clear_pin(FAN_PWM);
+    gpio_set_mode(PUMP_PWM, OUTPUT);
+    gpio_clear_pin(PUMP_PWM);
+    gpio_set_mode(COOL_EN, OUTPUT);
+    gpio_clear_pin(COOL_EN);
+    gpio_set_mode(HB_LED, OUTPUT);
+    gpio_clear_pin(HB_LED);
+
+    // Configure input GPIO pins
+    gpio_set_mode(SS_ESTOP_L, INPUT);
+    gpio_set_mode(SS_ESTOP_R, INPUT);
+    gpio_set_mode(SS_MC, INPUT);
+    gpio_set_mode(SS_GLVMS, INPUT);
+
+    // Initialize SPI bus
+    spi_init(&spi_cfg);
+
+    // Initialize CAN
+    can_init_pdu();
+    can_receive_dashboard();
+    can_receive_throttle();
+    can_receive_bspd();
+    can_receive_air_control_critical();
+    can_receive_bms_core();
+
+    // Initialize IO expander
     mcp23S17_init();
 
+    // Initialize display driver
     max7221_init();
+
+    // Initialize timers
+    sei();
+    timer_init(&timer_0_cfg); // Initialize main loop timer
+    timer_init(&timer_1_cfg); // Initialize water pump PWM timer
 }
 
-// Initialize IO expander
-void mcp23S17_init() {
-    // Set GPIO pins' direction to output
-    spi_transceive_custom_cs(MCP23S17_CS, (uint8_t[]){IO_DIRECTION_A, ALL_OUTPUT}, (uint8_t[]){0, 0}, 2);
-    spi_transceive_custom_cs(MCP23S17_CS, (uint8_t[]){IO_DIRECTION_B, ALL_OUTPUT}, (uint8_t[]){0, 0}, 2);
-}
-
-// Initialize display driver
-void max7221_init() {
-    // Leave shutdown mode
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){SHUTDOWN, SHUTDOWN_OFF}, (uint8_t[]){0, 0}, 2);
-    // Enable decoding on digits 3-0
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){DECODE, DECODE_4_DIGITS}, (uint8_t[]){0, 0}, 2);
-    // Set scan limit to display digits 3-0
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){SCAN_LIMIT, SCAN_4_DIGITS}, (uint8_t[]){0, 0}, 2);
-    // Set intensity to max brightness
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){INTENSITY, SET_MAX_BRIGHTNESS}, (uint8_t[]){0, 0}, 2);
-}
-
+// Function to test firmware - used for debugging.
 void hw_test(){
-    //// Test firmware
-    // Illuminate display
-    spi_transceive_custom_cs(MAX7221_CS, (uint8_t[]){DISPLAY_TEST, DISPLAY_TEST_ON}, (uint8_t[]){0, 0}, 2);
+    // // Illuminate display
+    // uint8_t txdata[2] = {DISPLAY_TEST, DISPLAY_TEST_ON};
+    // uint8_t rxdata = 0;
+    // spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+
     // Light up all shutdown LEDs
-    // Send CAN message with current readings
+    uint8_t txdata[3] = {OP_WRITE, IO_LATCH_A, 0xff};
+    uint8_t rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 3);
+    txdata[0] = OP_WRITE;
+    txdata[1] = IO_LATCH_B;
+    txdata[2] = 0xff;
+    rxdata = 0;
+    spi_transceive_custom_cs(MCP23S17_CS, txdata, &rxdata, 3);
+
+    // Perform current reading of ADC input 1 (service section)
+    // uint16_t reading = adc_read(INPUT_1);
+    // pdu_test.input1_current = reading;
+
+    // // Send CAN message
+    // can_send_pdu_test();
+}
+
+// Update tractive system status LEDs
+void update_ts_status(){
+    // Check CAN messages for BMS or AIR control faults
+    if (can_poll_receive_bms_core() == 0) {
+        // Check BMS for faults
+        can_receive_bms_core();
+    }
+    if (can_poll_receive_air_control_critical() == 0) {
+        // Check AIR control for faults
+        can_receive_air_control_critical();
+    }
+
+    if (bms_core.bms_state == BMS_STATE_FAULT || air_control_critical.air_fault == AIR_FAULT_IMD_STATUS) {
+        // Set TS status light to red if there is fault from either BMS or AIR control
+        gpio_clear_pin(TS_STATUS_G); // Turn off green light
+        gpio_set_pin(TS_STATUS_R); // Turn on red light
+    } else {
+        // Otherwise, set TS status light to green
+        gpio_clear_pin(TS_STATUS_R); // Turn off red light
+        gpio_set_pin(TS_STATUS_G); // Turn on green light
+    }
+}
+// ADC Read
+uint16_t adc_read(uint8_t input_pin){
+    // 32 clock cycles (4 bytes) are needed to read current on specified channel
+    uint8_t txdata[] = {input_pin, 0x00, input_pin, 0x00};
+    // Receive 4 bytes of data, but we only care about the last 12 bits
+    uint8_t rxdata[4];
+    spi_transceive_custom_cs(ADC1283_CS, txdata, rxdata, 4);
+    // Return the last 16 bits (only 12 bits contain data)
+    uint16_t reading = (rxdata[2] << 8) | rxdata[3];
+    // uint32_t reading = (rxdata[0] << 24) | (rxdata[1] << 16) | (rxdata[2] << 8) | rxdata[3];
+    return reading;
+}
+
+// Transmit current readings over CAN
+void transmit_currents() {
+    // List of all ADC inputs for current sensors on fuses
+    uint8_t adc_inputs[6] = {INPUT_0, INPUT_1, INPUT_2, INPUT_3, INPUT_6, INPUT_7};
+    uint8_t current_readings[6];
+
+    for (int i = 0; i < 6; i++){ // Repeat for all 6 inputs
+        uint16_t adc_reading = adc_read(adc_inputs[i]); // Read ADC
+
+        int16_t delta = (int16_t)adc_reading - QVO_COUNTS; // signed difference
+    
+        // Multiply by 10 before dividing to keep precision
+        int16_t current_x10 = (delta * 10) / ACS_SENS_COUNTS; 
+    
+        if (current_x10 < 0) { current_x10 = 0; } // Clip to >0
+        current_readings[i] = (uint8_t) MIN(current_x10, 255); // Clip to <255
+    }
+
+    // Assign readings to CAN message
+    pdu_currents.cooling_x10 = current_readings[0]; // Input 0
+    pdu_currents.service_section_x10 = current_readings[1]; // Input 1
+    pdu_currents.motor_controller_x10 = current_readings[2]; // Input 2
+    pdu_currents.lv_mobo_x10 = current_readings[3]; // Input 3
+    pdu_currents.shutdown_x10 = current_readings[4]; // Input 6
+    pdu_currents.pdu_x10 = current_readings[5]; // Input 7
+
+    // Send out current readings over CAN
+    can_send_pdu_currents();
 }
 
 int main(void) {
     hw_init();
-    hw_test();
 
-    // Main Loop: 
-    //  - Update shutdown node LEDs
-    //  - Display HV voltage/current on display
-    //  - Send CAN messages with current readings
-    //  - Cooling (???)
+    // Turn on pump and enable fans
+    gpio_set_pin(COOL_EN);
+
+    uint8_t heartbeat_counter = 0;
+
+    // Temp: Illuminate entire display so it doesn't flicker.
+    uint8_t txdata[2] = {DISPLAY_TEST, DISPLAY_TEST_ON};
+    uint8_t rxdata = 0;
+    spi_transceive_custom_cs(MAX7221_CS, txdata, &rxdata, 2);
+
+    // Main loop
+    while (true) {
+        if (run_10ms) { // Run every 10ms
+            // Update tractive system status LED
+            update_ts_status();
+
+            // Update shutdown node LEDs
+            // TODO: Read CAN messages and GPIO inputs to update the shutdown
+            // nodes. Control LEDs through SPI IO Expander. LED on = node
+            // closed, LED off = node open.
+
+            // Get current readings from ADC (+ publish to CAN)
+            transmit_currents();
+
+            // Update display
+            // TODO: display pack voltage (from V_sense ADC input), other things??
+
+            // Cooling logic
+            // TODO: route motor controller temp through a shutdown node, read
+            // value, then if temp above set fan control pin to high, 
+            // otherwise set fan control pin to low. Fan is not PWM.
+
+            // Heartbeat LED
+            run_10ms = false; // Set run flag to false
+
+            heartbeat_counter++;
+            if (heartbeat_counter == 50) { // Run every 500ms (2Hz)
+                heartbeat_counter = 0;
+                // Heartbeat LED
+                gpio_toggle_pin(HB_LED);
+            }
+        }
+    }
 }
--- vehicle/mkvii/software/integration/pdu/pdu.h
+++ vehicle/mkvii/software/integration/pdu/pdu.h
@@ -1,58 +1,136 @@
+#include <avr/interrupt.h>
 #include "libs/gpio/api.h"
 #include "libs/gpio/pin_defs.h"
 #include "libs/spi/api.h"
+#include "libs/timer/api.h"
+#include "vehicle/mkvii/software/pdu/can_api.h"
+
+// Macro that returns the minimum between two values
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+/*
+Transfer function constants for ACS37010LLZATR-030B5 current sensor and ADC1283 ADC.
+
+The ADC outputs raw digital values from 0–4095 (12-bit ADC). 
+Thus, we must convert voltage values into equivalent "ADC counts."
+
+ADC counts per volt for a 5V, 12-bit ADC:
+    4095 / 5V = 819 (ADC counts)/V
+
+For V_qvo = 2.5V (zero-current output voltage):
+    2.5V × 819 counts/V = 2047 ADC counts
+
+For ACS37010 sensitivity = 0.0667V/A:
+    0.0667 V/A × 819 counts/V ≈ 55 (ADC counts)/A
+*/
+#define QVO_COUNTS 2047 // V_qvo in ADC counts
+#define ACS_SENS_COUNTS 55 // ACS sensitivity in (ADC counts) / A
+
+// Pin definitions
+gpio_t MCP23S17_CS = PB3; // mcp23s17 - Chip select
+gpio_t ADC1283_CS = PB2; // adc1283 - Chip select
+gpio_t MAX7221_CS = PC5; // max7221 - Chip select
+gpio_t MCP23S17_RST = PB4; // mcp23s17 - Reset
+gpio_t TS_STATUS_G = PB5; // Output for tractive system status green LED
+gpio_t TS_STATUS_R = PB6; // Output for tractive system status red LED
+gpio_t FAN_PWM = PD0; // Output for PWM signal to fan
+gpio_t PUMP_PWM = PD1; // Output for PWM signal to pump
+gpio_t COOL_EN = PD5; // Output to enable cooling system
+gpio_t HB_LED = PD7; // Output for heartbeat LED
+gpio_t SS_ESTOP_L = PC0; // Input for service section left estop
+gpio_t SS_ESTOP_R = PC6; // Input for service section right estop
+gpio_t SS_MC = PC1; // Input for motor controller high voltage interlock
+gpio_t SS_GLVMS = PC7; // Input for main fuse 
+
+
+// Opcodes for mcp23s17
+typedef enum {
+    OP_WRITE = 0x40,
+    OP_READ = 0x41,
+} mcp23s17_opcode;
 
 // Addresses for mcp23s17 (Assumes IOCON.BANK = 0)
-enum mcp23s17_address {
+typedef enum {
     IO_DIRECTION_A = 0x00,
     IO_DIRECTION_B = 0x01,
-};
+    IO_LATCH_A = 0x14,
+    IO_LATCH_B = 0x15, 
+} mcp23s17_address;
 
 // Commands for mcp23s17
-enum mcp23s17_command {
+typedef enum {
     ALL_OUTPUT = 0x00,
-};
-
-// Addresses for adc1283
-enum adc1283_address {
-    ADC_CONTROL = 0x00, // Could not find address in datasheet, assuming it is 0x00
-};
+} mcp23s17_command;
 
 // Commands for adc1283
-enum adc1283_command {
-    INPUT_0 = 0x00,
-    INPUT_1 = 0x08,
-    INPUT_2 = 0x10,
-    INPUT_3 = 0x18,
-    INPUT_4 = 0x20,
-    INPUT_5 = 0x28,
-    INPUT_6 = 0x30,
-    INPUT_7 = 0x38,
-};
+typedef enum {
+    INPUT_0 = 0x00, // Current at cooling fuse
+    INPUT_1 = 0x08, // Current at service section fuse
+    INPUT_2 = 0x10, // Current at motor controller fuse
+    INPUT_3 = 0x18, // Current at LV motherboard fuse
+    INPUT_4 = 0x20, // Voltage at main fuse
+    INPUT_5 = 0x28, // Temperature signal from cooling
+    INPUT_6 = 0x30, // Current at shutdown fuse (roll hoop)
+    INPUT_7 = 0x38, // Current at PDU fuse
+} adc1283_command;
 
 // Addresses for max7211
-enum max7221_address {
+typedef enum {
     DECODE = 0x09,
     INTENSITY = 0x0A,
     SCAN_LIMIT = 0x0B,
     SHUTDOWN = 0x0C,
     DISPLAY_TEST = 0x0F,
-};
+} max7221_address;
 
 // Commands for max7221
-enum max7221_command{
+typedef enum {
     DECODE_4_DIGITS = 0x0F,
     SCAN_4_DIGITS = 0x03,
     SET_MAX_BRIGHTNESS = 0x0F,
     SHUTDOWN_OFF = 0x01,
     DISPLAY_TEST_ON = 0x01,
+} max7221_command;
+
+// 10ms main loop timer config
+void timer_0_isr(void);
+timer_cfg_s timer_0_cfg = {
+    .timer = TIMER0,
+    .timer0_mode = TIMER0_MODE_CTC,
+    .prescalar = CLKIO_DIV_1024,
+    .channel_a = {
+        .channel = CHANNEL_A,
+        .output_compare_match = 0x27, // 100 Hz
+        .pin_behavior = DISCONNECTED,
+        .interrupt_enable = true,
+        .interrupt_callback = timer_0_isr,
+    },
 };
 
-// Chip Select Pins
-gpio_t MCP23S17_CS = PB3; 
-gpio_t ADC1283_CS = PB2; 
-gpio_t MAX7221_CS = PC5; 
+/* 
+Water pump PWM config: 100 Hz 50% duty cycle
 
+On MKVII, the pump PWM signal pin is not connected to the pins controlled by 
+Timer 1 (PDU design error), which means that we cannot use the hardware PWM to
+control the pump. Instead we implement a software PWM by using a compare match
+timer with an interrupt.
+*/
+void timer_1_isr(void);
+timer_cfg_s timer_1_cfg = {
+    .timer = TIMER1,
+    .timer1_mode = TIMER1_MODE_CTC,
+    .prescalar = CLKIO_DIV_1024,
+    .channel_a = {
+        .channel = CHANNEL_A,
+        .output_compare_match = 0x4E, // 100 Hz
+        .pin_behavior = DISCONNECTED,
+        .interrupt_enable = true,
+        .interrupt_callback = timer_1_isr,
+    },
+};
+
+
+// SPI config struct
 spi_cfg_s spi_cfg = {
     .interrupt_enable = false,
     .data_order = MSB,
@@ -60,7 +138,7 @@ spi_cfg_s spi_cfg = {
     .polarity = FALLING_RISING,
     .phase = SETUP_SAMPLE,
     .clock_rate = F_OSC_DIV_16,
-    .cs_pin = PB3, 
+    .cs_pin = &MAX7221_CS, 
     // Ignore the chip select pin since we are using custom transcieve
     // function to interface with multiple periphreal devices
 };
